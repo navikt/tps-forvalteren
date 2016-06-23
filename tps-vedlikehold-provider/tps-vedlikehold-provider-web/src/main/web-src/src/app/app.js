9c512f1ddec58911ab1ae782443150c6d0f1fa0c
unknown <G148235@A34DUVW25889.devillo.no>
2016-06-23 17:01:43 +0200

    Add basic routing and templates

--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/app.js
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/app.js
@@ -1,4 +1,29 @@
 /**
  * Created by k148233 on 23.06.2016.
  */
-console.log("hello from app script");
+require('angular');
+require('angular-ui-router');
+require('angular-animate');
+require('angular-material');
+
+
+//require('./components/login/login');
+
+var app = angular.module('tps-vedlikehold', ['ui.router', 'ngMaterial']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouteProvider) {
+
+    $urlRouteProvider.otherwise("/");
+
+    $stateProvider
+
+    .state('login', {
+        url: "/login",
+        templateUrl: "app/components/login/login.html"
+    })
+    .state('dashboard', {
+        url: "/dashboard",
+        templateUrl: "app/components/dashboard/dashboard.html"
+    });
+
+}]);
