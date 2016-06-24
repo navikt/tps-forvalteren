dc23f7aea909dd32409708c9d9411936b010d219
Øyvind Grimnes <oyvind.grimnes@nav.no>
2016-06-24 09:50:19 +0200

    PKJAG-2397 Added swagger and swagger ui to the project

---
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/main/java/no/nav/tps/vedlikehold/provider/rs/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package no.nav.tps.vedlikehold.provider.rs.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContext;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Configure automated swagger API documentation
+ *
+ * @author Øyvind Grimnes, Visma Consulting AS
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("api/v1/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Glossy API",
+                "TPS Vedlikeholdsklient API",
+                "Pusekatt",
+                "TOS",
+                new Contact("Pusekatt", "stash.devillo.no", "oyvind.grimnes@nav.no"),
+                "MIT License",
+                "https://opensource.org/licenses/MIT"
+        );
+    }
+}
