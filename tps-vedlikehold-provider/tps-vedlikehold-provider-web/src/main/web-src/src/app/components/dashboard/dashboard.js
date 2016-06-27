92485220027d375e4560c3a33ee033e32bb3df7a
Kristian Kyvik <kristian.kyvik@nav.no>
2016-06-27 08:28:45 +0200

    PKJAG-2391 Uncommented gulp task that minifies javascript files

--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/components/dashboard/dashboard.js
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/components/dashboard/dashboard.js
@@ -1,9 +1,6 @@
 /**
  * Created by K148233 on 24.06.2016.
  */
-/**
- * Created by G148235 on 24.06.2016.
- */
 angular.module('tps-vedlikehold.dashboard', ['ngMessages'])
     .controller('dashboardCtrl', ['$scope', 'sessionService',
         function($scope, sessionService) {
