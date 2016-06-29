86c072529a3c1a265e061420c83da7b94c6b32c1
Kristian Kyvik <kristian.kyvik@nav.no>
2016-06-29 14:33:33 +0200

    PKJAG-2386 Added correct @author tag

--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/services/locationService.js
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/services/locationService.js
@@ -1,5 +1,5 @@
 /**
- * Created by G148235 on 24.06.2016.
+ * @author Frederik Gørvell (Visma Consulting AS).
  */
 angular.module('tps-vedlikehold.service')
     .service('locationService', ['$rootScope', '$location', function($rootScope, $location) {
