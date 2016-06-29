6c03b0558078908246616a55904853c28d14e151
Kristian Kyvik <kristian.kyvik@nav.no>
2016-06-29 12:42:48 +0200

    PKJAG-2386 Added  service to authservice. API route prefix now dynamic

--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/services/AuthenticationService.js
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/services/AuthenticationService.js
@@ -1,46 +1,42 @@
 angular.module('tps-vedlikehold.service')
-    .service('authenticationService', ['$http', 'sessionService', function($http, sessionService) {
+    .service('authenticationService', ['$http', '$location', 'sessionService', function($http, $location, sessionService) {
 
         var self = this;
 
-        var routeDevPrefix = 'http://localhost:8080';
-
-        var loginRoute = routeDevPrefix+'/api/v1/user';
-        var logoutRoute = routeDevPrefix+'/api/v1/user/logout';
+        var routePrefix = $location.protocol() + '://' + $location.host() + ':' + $location.port();
+        var loginRoute = routePrefix + '/api/v1/user';
+        var logoutRoute = routePrefix + '/api/v1/user/logout';
 
         self.authenticate = function(credentials, callback) {
-            
+
             var headers = credentials ?
             {'Authorization': 'Basic ' + btoa(credentials.username + ":" + credentials.password)} : {};
 
             $http.get(loginRoute, {
                 headers: headers
             })
-            .then(function(res) {
-                sessionService.setIsAuthenticated(true);
-                sessionService.setIsSignedIn(true);
-                sessionService.setCurrentUser(res.data);
-                sessionService.setToken(res.data.token);
+            .then(function (res) {
+                    sessionService.setIsAuthenticated(true);
+                    sessionService.setIsSignedIn(true);
+                    sessionService.setCurrentUser(res.data);
+                    sessionService.setToken(res.data.token);
 
-                if (callback) {
-                    callback(res);
-                }
-            },
-            function(res) {
+                    if (callback) {
+                        callback(res);
+                    }
+                },
+                function (res) {
 
-                if (callback) {
-                    callback(res);
-                }
-            });
+                    if (callback) {
+                        callback(res);
+                    }
+                });
         };
 
         self.validateToken = function(){
             self.authenticate();
         };
 
-        self.isAuthorized = function() {
-            
-        };
 
         self.invalidateSession = function(callback){
             sessionService.setIsAuthenticated(false);
