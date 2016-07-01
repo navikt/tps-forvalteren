6d0498b96b9354b7ca94225ba54049a055a42321
Øyvind Grimnes <oyvind.grimnes@nav.no>
2016-07-01 13:56:18 +0200

    PKJAG-2503 Added test for TPSWS queue consumer bean initialisation. Makes sure the bean is autowired before it is returned

---
+++ tps-vedlikehold-consumer/tps-vedlikehold-consumer-rs/src/test/java/no/nav/tps/vedlikehold/consumer/rs/fasit/FasitConfigTest.java
@@ -0,0 +1,31 @@
+package no.nav.tps.vedlikehold.consumer.rs.fasit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Øyvind Grimnes, Visma Consulting AS
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class FasitConfigTest {
+
+    @Mock
+    private AutowireCapableBeanFactory beanFactoryMock;
+
+    @InjectMocks
+    private FasitConfig fasitConfig;
+
+    @Test
+    public void getTpswsFasitWueueConsumerAutowiresTheInstatiatedConsumer() {
+        fasitConfig.getTpswsFasitQueueConsumer();
+        verify(beanFactoryMock).autowireBean( isA(FasitQueueConsumer.class) );
+    }
+}
