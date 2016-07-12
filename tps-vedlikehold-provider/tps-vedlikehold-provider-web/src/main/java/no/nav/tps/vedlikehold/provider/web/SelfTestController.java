50c4589a95a3584f735d2982e6b07a4b74626477
Kristian Kyvik <kristian.kyvik@nav.no>
2016-07-12 15:09:25 +0200

    PKJAG-2666 Added bootstrap webjar plus boostrap classes to selftest.html

--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/java/no/nav/tps/vedlikehold/provider/web/SelfTestController.java
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/java/no/nav/tps/vedlikehold/provider/web/SelfTestController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -18,15 +20,23 @@ import static java.util.Arrays.asList;
 @Controller
 public class SelftestController {
 
+    @Value("${application.descriptiveName:MISSING_DESCRIPTIVE_APPLICATION_NAME}")
+    private String descriptiveApplicationName;
+
     @Value("${application.name:MISSING_APPLICATION_NAME}")
     private String applicationName;
 
     @Value("${application.version:MISSING_APPLICATION_VERSION}")
     private String applicationVersion;
 
+    @Value("${bootstrap.version:MISSING_BOOTSTRAP_VERSION}")
+    private String bootstrapVersion;
+
     @RequestMapping("/internal/selftest")
     public String selftest(@RequestParam(value = "status", required = false) String status, Model model) {
         model.addAttribute("applicationProperties", collectApplicationProperties());
+        model.addAttribute("applicationName", descriptiveApplicationName);
+        model.addAttribute("bootstrapVersion", bootstrapVersion);
         return "selftest";
 
     }
@@ -35,8 +45,17 @@ public class SelftestController {
 
         return asList(
                 new ApplicationProperty("selftest.application.name", applicationName),
-                new ApplicationProperty("selftest.application.version", applicationVersion)
+                new ApplicationProperty("selftest.application.version", applicationVersion),
+                new ApplicationProperty("selftest.application.server", getServerAddress())
         );
+    }
+
+    private String getServerAddress() {
+        try {
+            return InetAddress.getLocalHost().getCanonicalHostName();
+        } catch (UnknownHostException e) {
+            return "N/A";
+        }
     }
 
 }
