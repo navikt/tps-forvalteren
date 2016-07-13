92d1c1bc1aa3955362f949a073af703334dac1d0
Kristian Kyvik <kristian.kyvik@nav.no>
2016-07-13 10:22:16 +0200

    PKJAG-2666 Added Selftest methods to SelftestController

--- tps-vedlikehold-common/tps-vedlikehold-common-java/src/main/java/no/nav/tps/vedlikehold/common/java/message/MessageConstants.java
+++ tps-vedlikehold-common/tps-vedlikehold-common-java/src/main/java/no/nav/tps/vedlikehold/common/java/message/MessageConstants.java
@@ -4,6 +4,6 @@ package no.nav.tps.vedlikehold.common.java.message;
  * @author Kristian Kyvik (Visma Consulting AS).
  */
 public class MessageConstants {
-
     public static final String SELFTEST_UNKNOWN_ERROR_KEY = "selftest.result.unknown.error";
+    public static final String SELFTEST_EXCEPTION_MESSAGE_KEY = "selftest.result.exception.message";
 }
--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/java/no/nav/tps/vedlikehold/provider/web/SelfTestController.java
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/java/no/nav/tps/vedlikehold/provider/web/SelfTestController.java
@@ -1,18 +1,30 @@
 package no.nav.tps.vedlikehold.provider.web;
 
-import no.nav.tps.vedlikehold.provider.web.model.ApplicationProperty;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import static org.springframework.util.StringUtils.collectionToCommaDelimitedString;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
 import static java.util.Arrays.asList;
+
+import static no.nav.tps.vedlikehold.common.java.message.MessageConstants.SELFTEST_EXCEPTION_MESSAGE_KEY;
+import static no.nav.tps.vedlikehold.provider.web.model.SelftestResult.Status.FEILET;
+
+import no.nav.tps.vedlikehold.common.java.message.MessageProvider;
+import no.nav.tps.vedlikehold.provider.web.model.SelftestResult;
+import no.nav.tps.vedlikehold.provider.web.selftest.Selftest;
+import no.nav.tps.vedlikehold.provider.web.exception.SelftestFailureException;
+import no.nav.tps.vedlikehold.provider.web.model.AggregateSelftestResult;
+import no.nav.tps.vedlikehold.provider.web.model.ApplicationProperty;
 
 /**
  * @author Kristian Kyvik (Visma Consulting).
@@ -32,13 +44,32 @@ public class SelftestController {
     @Value("${bootstrap.version:MISSING_BOOTSTRAP_VERSION}")
     private String bootstrapVersion;
 
+    @Autowired
+    @Qualifier(value = "repositorySelftest")
+    private Selftest egenAnsattSelftest;
+
+    @Autowired
+    @Qualifier(value = "fellesKodeverkSelftest")
+    private Selftest DiskresjonskodeSelftest;
+
+    @Autowired
+    private MessageProvider messageProvider;
+
     @RequestMapping("/internal/selftest")
     public String selftest(@RequestParam(value = "status", required = false) String status, Model model) {
-        model.addAttribute("applicationProperties", collectApplicationProperties());
-        model.addAttribute("applicationName", descriptiveApplicationName);
-        model.addAttribute("bootstrapVersion", bootstrapVersion);
-        return "selftest";
+        List<SelftestResult> selftestResults = collectSelftestResults();
 
+        if (status != null && containsFailures(selftestResults)) {
+            String message = messageProvider.get(SELFTEST_EXCEPTION_MESSAGE_KEY, mergeFailedSubSystemNames(selftestResults));
+            throw new SelftestFailureException(message);
+        } else {
+            model.addAttribute("applicationProperties", collectApplicationProperties());
+            model.addAttribute("applicationName", descriptiveApplicationName);
+            model.addAttribute("bootstrapVersion", bootstrapVersion);
+            model.addAttribute("aggregateStatus", aggregateSelftestResults(selftestResults));
+            model.addAttribute("selftestResults", selftestResults);
+            return "selftest";
+        }
     }
 
     private List<ApplicationProperty> collectApplicationProperties() {
@@ -56,6 +87,41 @@ public class SelftestController {
         } catch (UnknownHostException e) {
             return "N/A";
         }
+    }
+
+    private SelftestResult.Status aggregateSelftestResults(List<SelftestResult> results) {
+        AggregateSelftestResult aggregateSelftestResult = new AggregateSelftestResult();
+        aggregateSelftestResult.addResults(results);
+        return aggregateSelftestResult.getStatus();
+    }
+
+    private List<SelftestResult> collectSelftestResults() {
+        return Collections.unmodifiableList(asList(
+                DiskresjonskodeSelftest.perform(),
+                egenAnsattSelftest.perform()
+        ));
+    }
+
+    private boolean containsFailures(List<SelftestResult> results) {
+        for (SelftestResult result : results) {
+            if (result.getStatus() == FEILET) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String mergeFailedSubSystemNames(List<SelftestResult> results) {
+        List<String> failedSubSystemNames = new ArrayList<>();
+
+        for (SelftestResult result : results) {
+            if (result.getStatus() == FEILET) {
+                failedSubSystemNames.add(result.getName());
+            }
+        }
+
+        return collectionToCommaDelimitedString(failedSubSystemNames);
     }
 
 }
