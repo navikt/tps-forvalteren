db54f51bf7eac283170acd83a67af9fac1c3cba9
Kristian Kyvik <kristian.kyvik@nav.no>
2016-07-14 14:16:10 +0200

    PKJAG-2666 Added tests for model class

---
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/test/java/no/nav/tps/vedlikehold/web/model/SelftestResultTest.java
@@ -0,0 +1,46 @@
+package no.nav.tps.vedlikehold.web.model;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import no.nav.tps.vedlikehold.provider.web.model.SelftestResult;
+import static no.nav.tps.vedlikehold.provider.web.model.SelftestResult.Status.FEILET;
+import static no.nav.tps.vedlikehold.provider.web.model.SelftestResult.Status.OK;
+
+/**
+ * @author Kristian Kyvik (Visma Consulting AS).
+ */
+public class SelftestResultTest {
+
+    @Test
+    public void constructorWithOneParameterInitializesNameAndOkStatus() {
+        SelftestResult result = new SelftestResult("Result name");
+
+        assertThat(result.getName(), is(equalTo("Result name")));
+        assertThat(result.getStatus(), is(OK));
+        assertThat(result.getResponseTime(), is(0L));
+        assertThat(result.getErrorMessage(), is(equalTo("")));
+    }
+
+    @Test
+    public void constructorWithOneParameterInitializesNameAndFeiletStatus() {
+        SelftestResult result = new SelftestResult("Result name", "Error message");
+
+        assertThat(result.getName(), is(equalTo("Result name")));
+        assertThat(result.getStatus(), is(FEILET));
+        assertThat(result.getResponseTime(), is(0L));
+        assertThat(result.getErrorMessage(), is(equalTo("Error message")));
+    }
+
+    @Test
+    public void setsResponseTime() {
+        SelftestResult result = new SelftestResult("Result name");
+
+        result.setResponseTime(1337L);
+
+        assertThat(result.getResponseTime(), is(1337L));
+    }
+}
