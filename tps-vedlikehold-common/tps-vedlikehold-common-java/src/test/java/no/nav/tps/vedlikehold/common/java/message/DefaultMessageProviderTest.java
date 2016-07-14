23ca2299c83fc720dd1b0bc01e54fb6c4e03bd60
Kristian Kyvik <kristian.kyvik@nav.no>
2016-07-14 16:03:19 +0200

    PKJAG-2666 Added LoggerTestutil class

---
+++ tps-vedlikehold-common/tps-vedlikehold-common-java/src/test/java/no/nav/tps/vedlikehold/common/java/message/DefaultMessageProviderTest.java
@@ -0,0 +1,7 @@
+package no.nav.tps.vedlikehold.common.java.message;
+
+/**
+ * Created by K148233 on 14.07.2016.
+ */
+public class DefaultMessageProviderTest {
+}
---
+++ tps-vedlikehold-common/tps-vedlikehold-common-test/src/main/java/no/nav/tps/vedlikehold/common/test/util/LoggerTestUtils.java
@@ -0,0 +1,7 @@
+package no.nav.tps.vedlikehold.common.test.util;
+
+/**
+ * Created by K148233 on 14.07.2016.
+ */
+public class LoggerTestUtils {
+}
