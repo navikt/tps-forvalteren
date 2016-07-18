bbcd8b8072789bd59a1e53ea309f24fcc5709963
Øyvind Grimnes <oyvind.grimnes@nav.no>
2016-07-18 14:56:44 +0200

    GKJAG-2658 Organized dependencies

--- tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/test/java/no/nav/tps/vedlikehold/provider/rs/api/v1/endpoints/ServiceControllerTest.java
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/test/java/no/nav/tps/vedlikehold/provider/rs/api/v1/endpoints/ServiceControllerTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.mockito.Mockito.*;
