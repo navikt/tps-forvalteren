f4cef6196da7ef9e12a93017c742a034c5c00aa0
Kristian Kyvik <kristian.kyvik@nav.no>
2016-07-18 15:56:11 +0200

    PKJAG-2666 Deleted selftest controller integration tests

--- tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/main/java/no/nav/tps/vedlikehold/provider/rs/config/RestProviderConfig.java
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/main/java/no/nav/tps/vedlikehold/provider/rs/config/RestProviderConfig.java
@@ -5,10 +5,7 @@ import no.nav.tps.vedlikehold.provider.rs.api.v1.documentation.SwaggerConfig;
 import no.nav.tps.vedlikehold.provider.rs.api.v1.endpoints.UserController;
 import no.nav.tps.vedlikehold.provider.rs.security.config.RestSecurityConfig;
 import no.nav.tps.vedlikehold.provider.rs.security.config.WebSecurityConfig;
-<<<<<<< HEAD:tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/main/java/no/nav/tps/vedlikehold/provider/rs/RestProviderConfig.java
-=======
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
->>>>>>> b8f7c02c26f7c4624ef96b15c6905f81edb581e2:tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/main/java/no/nav/tps/vedlikehold/provider/rs/config/RestProviderConfig.java
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -26,9 +23,6 @@ import org.springframework.context.annotation.Import;
 @ComponentScan(basePackageClasses = {
         UserController.class
 })
-<<<<<<< HEAD:tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/main/java/no/nav/tps/vedlikehold/provider/rs/RestProviderConfig.java
 public class RestProviderConfig {
 }
-=======
-public class RestProviderConfig {}
->>>>>>> b8f7c02c26f7c4624ef96b15c6905f81edb581e2:tps-vedlikehold-provider/tps-vedlikehold-provider-rs/src/main/java/no/nav/tps/vedlikehold/provider/rs/config/RestProviderConfig.java
+
