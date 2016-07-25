3c6e8b53a0ccf1b476145f6b78062c66c6460552
Øyvind Grimnes <oyvind.grimnes@nav.no>
2016-07-25 15:37:28 +0200

    Added a few comments

--- tps-vedlikehold-consumer/tps-vedlikehold-consumer-mq/src/main/java/no/nav/tps/vedlikehold/consumer/mq/config/MessageQueueConsumerConstants.java
+++ tps-vedlikehold-consumer/tps-vedlikehold-consumer-mq/src/main/java/no/nav/tps/vedlikehold/consumer/mq/config/MessageQueueConsumerConstants.java
@@ -5,8 +5,10 @@ package no.nav.tps.vedlikehold.consumer.mq.config;
  */
 public class MessageQueueConsumerConstants {
 
+    /* Used to connect to the QueueManager */
     public static final String USERNAME = "srvappserver";
     public static final String PASSWORD = "";
 
+    /* Used to create a channel name by combining it with the environment name. E.g. 'T1_TPSWS'*/
     public static final String CHANNEL_POSTFIX = "_TPSWS";
 }
