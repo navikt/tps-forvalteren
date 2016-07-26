62932e882dc9cb87a64dca5fb848c7d159a42a87
Øyvind Grimnes <oyvind.grimnes@nav.no>
2016-07-26 11:13:22 +0200

    Removed unused domain-rs

--- tps-vedlikehold-consumer/tps-vedlikehold-consumer-mq/src/main/java/no/nav/tps/vedlikehold/consumer/mq/factories/strategies/ConnectionFactoryFactoryStrategy.java
+++ tps-vedlikehold-consumer/tps-vedlikehold-consumer-mq/src/main/java/no/nav/tps/vedlikehold/consumer/mq/factories/strategies/ConnectionFactoryFactoryStrategy.java
@@ -1,9 +1,11 @@
 package no.nav.tps.vedlikehold.consumer.mq.factories.strategies;
 
 /**
+ * Provides information needed when creating a queue connection factory
+ *
  * @author Øyvind Grimnes, Visma Consulting AS
  */
-public interface ConnectionFactoryStrategy {
+public interface ConnectionFactoryFactoryStrategy {
     String getName();
     String getHostName();
     String getChannelName();
