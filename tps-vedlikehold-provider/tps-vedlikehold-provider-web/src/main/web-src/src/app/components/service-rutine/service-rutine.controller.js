bc741f3372ab5e339a8ddaf9c0fe3e0d50b36545
Fløgstad <F148888@ADEO.NO>
2016-10-13 14:55:57 +0200

    Adjusted UI and added some methods to the controller

--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/components/service-rutine/service-rutine.controller.js
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/src/app/components/service-rutine/service-rutine.controller.js
@@ -3,7 +3,7 @@
  */
 angular.module('tps-vedlikehold.service-rutine')
     .controller('ServiceRutineCtrl', ['$scope', '$stateParams', '$mdDialog', '$document', 'utilsService', 'serviceRutineFactory', 'responseFormConfig', 'environmentsPromise',
-        function($scope, $stateParams, $mdDialog, $document, utilsService, serviceRutineFactory, responseFormConfig, environmentsPromise) {
+        function ($scope, $stateParams, $mdDialog, $document, utilsService, serviceRutineFactory, responseFormConfig, environmentsPromise) {
 
             $scope.serviceRutineName = $stateParams.serviceRutineName;
             $scope.loading = false;
@@ -14,7 +14,8 @@ angular.module('tps-vedlikehold.service-rutine')
             $scope.onlyNumbers = /^\d+$/;
             $scope.onlyLetters = /^[a-zA-Z0-9\s]*$/;
             $scope.personsData = {};
-
+            $scope.toggle = false;
+            $scope.buffNumbers = ["1"];
 
             var tpsReturnedObject = {};
             var nonUniqueProperties = []; //objects that contain non-unique properties
@@ -26,64 +27,117 @@ angular.module('tps-vedlikehold.service-rutine')
                 return isValidServiceRutineName && !apiError;
             };
 
-            $scope.submit = function() {
+            $scope.submit = function () {
                 var params = createParams($scope.formData);
                 $scope.loading = true;
 
-                serviceRutineFactory.getResponse($scope.serviceRutineName, params).then(function(response) {
+                serviceRutineFactory.getResponse($scope.serviceRutineName, params).then(function (response) {
                     $scope.loading = false;
+                    $scope.clearResponseForm();
+
                     $scope.xmlForm = utilsService.formatXml(response.data.xml);
 
                     tpsReturnedObject = response.data.data.tpsPersonData;
 
                     var svarStatus = tpsReturnedObject.tpsSvar.svarStatus;
-                    $scope.svarStatus = "STATUS: " + svarStatus.returStatus + " " +  svarStatus.returMelding + " " +  svarStatus.utfyllendeMelding;
+                    $scope.svarStatus = "STATUS: " + svarStatus.returStatus + " " + svarStatus.returMelding + " " + svarStatus.utfyllendeMelding;
                     $scope.returStatus = svarStatus.returStatus;
 
                     var jsonObjectWithResultData = tpsReturnedObject.tpsSvar[serviceRutineFactory.getServiceRutineReturnedDataLabel($scope.serviceRutineName)];
-                    extractPersonsData(jsonObjectWithResultData, $scope.serviceRutineName, nonUniqueProperties);
+                    if(jsonObjectWithResultData === undefined) return;
+                    $scope.personsData = extractPersonsData(jsonObjectWithResultData, nonUniqueProperties);
+                    capitalizeFirstLetterInPersonsData(jsonObjectWithResultData);
+                    var antallTreff = jsonObjectWithResultData.antallTotalt;
+                    if(antallTreff === undefined || antallTreff == 1) $scope.toggle = true;
+                    else updateBufferChoices(antallTreff);
 
-                }, function(error) {
+                }, function (error) {
                     $scope.loading = false;
                     showAlertTPSError(error);
                 });
             };
 
-
-            $scope.clearResponseForm = function() {
+            $scope.clearResponseForm = function () {
                 $scope.personsData = {};
+                $scope.toggle = false;
                 $scope.svarStatus = null;
                 $scope.xmlForm = null;
             };
 
-            $scope.isRequired = function(type) {
+            $scope.isRequired = function (type) {
                 return (requiredParameters.indexOf(type) > -1);
             };
 
-            $scope.repositionDatePicker = function(pika){
+            $scope.repositionDatePicker = function (pika) {
                 var parentScrollOffset = Math.abs(parseInt($document[0].body.style.top));
                 parentScrollOffset = parentScrollOffset ? parentScrollOffset : 0;
                 var modalInputOffset = pika.el.offsetTop;
-                pika.el.style.top = parentScrollOffset+modalInputOffset+'px';
+                pika.el.style.top = parentScrollOffset + modalInputOffset + 'px';
             };
 
-            function extractPersonsData(responseObject, serviceRutineName, nonUniqueProperties){
-                switch(serviceRutineName){
-                    case "FS03-FDNUMMER-KONTINFO-O":
-                        $scope.personsData[0] = utilsService.flattenObject(responseObject, nonUniqueProperties);
-                        break;
-                    case "FS03-FDNUMMER-PERSDATA-O":
-                        $scope.personsData[0] = utilsService.flattenObject(responseObject, nonUniqueProperties);
-                        break;
-                    case "FS03-NAADRSOK-PERSDATA-O":
-                        var i = 0;
-                        while(i<responseObject.antallTotalt){
-                            $scope.personsData[i] = utilsService.flattenObject(responseObject.enPersonRes[i], nonUniqueProperties);
-                            i++;
-                        }
-                        break;
-                    default:        // Default for nå. Må sikkert gjøre om.
+            $scope.getAntallTreff = function(object){
+                var count = 0;
+                for(var key in object){
+                    count++;
                 }
+                return count;
+            };
+
+            $scope.pageForward = function pageForward(){
+               $scope.formData.buffNr = (parseInt($scope.formData.buffNr)+1).toString();
+                $scope.submit();
+            };
+
+            $scope.pageBackwards = function pageBackwards(){
+                $scope.formData.buffNr = (parseInt($scope.formData.buffNr)-1).toString();
+                $scope.submit();
+            };
+
+            //TODO Bytt navn til noe mer beskrivende.
+            $scope.getStartIndex = function (buffer){
+                var i = parseInt(buffer);
+                return ((i-1)*34 + 1);
+            };
+
+            $scope.getEndIndex = function (buffer, antallTreff){
+                var bufferNummer = parseInt(buffer);
+                if(bufferNummer*34 > antallTreff) return antallTreff;
+                return 34*bufferNummer;
+            };
+
+            function capitalizeFirstLetterInPersonsData(responseObject){
+                if (responseObject.antallTotalt === undefined) {
+                    $scope.personsData[0] = utilsService.capitalizeFirstLetterInObjectProperties($scope.personsData[0]);
+                } else {
+                    var i = 0;
+                    while (i < responseObject.antallTotalt) {
+                        $scope.personsData[i] = utilsService.capitalizeFirstLetterInObjectProperties($scope.personsData[i]);
+                        i++;
+                    }
+                }
+            }
+
+            function updateBufferChoices(antallTreff){
+                var i = 0;
+                $scope.buffNumbers = [];
+                while(i < Math.ceil(parseInt(antallTreff)/34)){
+                    $scope.buffNumbers[i] = (i+1).toString();
+                    i++;
+                }
+            }
+
+            function extractPersonsData(responseObject, nonUniqueProperties) {
+                var personsData = {};
+                if (responseObject.antallTotalt === undefined || responseObject.antallTotalt == 1) {
+                    personsData[0] = utilsService.flattenObject(responseObject, nonUniqueProperties);
+                } else {
+                    var i = 0;
+                    while (i < responseObject.antallTotalt) {
+                        personsData[i] = utilsService.flattenObject(responseObject.enPersonRes[i], nonUniqueProperties);
+                        i++;
+                    }
+                }
+                return personsData;
             }
 
             function showAlertTPSError(error) {
@@ -156,22 +210,22 @@ angular.module('tps-vedlikehold.service-rutine')
 
             // This is needed in order to force the first tab to focus after refresh when navigating using the tab key.
             function overwriteTabFocusBehaviour() {
-                angular.element(document).ready(function() {
+                angular.element(document).ready(function () {
                     var mdTabsCanvas = angular.element(document.querySelector(".tps-vk-scrollable-tabs"))[0].children[0].children[1];
                     var firstTab = mdTabsCanvas.children[0].children[0];
                     var done = false;
 
-                    mdTabsCanvas.addEventListener("focusin", function(e){
+                    mdTabsCanvas.addEventListener("focusin", function (e) {
                         if (!done) {
                             firstTab.classList.add("md-focused");
                             done = true;
                         }
-                    } );
+                    });
 
 
-                    mdTabsCanvas.addEventListener("focusout", function(e){
+                    mdTabsCanvas.addEventListener("focusout", function (e) {
                         firstTab.classList.remove("md-focused");
-                    } );
+                    });
 
                 });
             }
@@ -189,24 +243,19 @@ angular.module('tps-vedlikehold.service-rutine')
             function initRequestForm() {
                 for (var i = 0; i < $scope.fields.length; i++) {
                     var parameter = $scope.fields[i];
-                    switch(parameter) {
-                        case 'fnr':
-                            $scope.formData.fnr = '';
-                            break;
+                    switch (parameter) {
                         case 'aksjonsDato':
                             $scope.formData.aksjonsDato = utilsService.getCurrentFormattedDate();
                             break;
                         case 'aksjonsKode':
                             $scope.formData.aksjonsKode = $scope.selectValues.aksjonsKode[0];
                             break;
-                        case 'navn':
-                            $scope.formData.navn = '';
-                            break;
                         default:
                             $scope.formData[parameter] = '';
                     }
                 }
                 $scope.formData.environment = $scope.environments ? $scope.environments[0] : null;
+                $scope.formData.buffNr = $scope.buffNumbers ? $scope.buffNumbers[0] : null;
             }
 
             // ##################################
