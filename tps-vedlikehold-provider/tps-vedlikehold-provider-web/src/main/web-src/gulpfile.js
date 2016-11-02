ed1c883f4e0c15951d2032359a51dea5bac666d9
Filip Fossum <filip.fossum@nav.no>
2016-11-02 17:49:27 +0100

    endret byggrutine for frontend. cachebusting og minification kun for prodbygg. proxy devserver med livereload. Oppdatert avhengigheter.

--- tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/gulpfile.js
+++ tps-vedlikehold-provider/tps-vedlikehold-provider-web/src/main/web-src/gulpfile.js
@@ -1,142 +1,234 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
-var browserify = require('gulp-browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var jshint = require('gulp-jshint');
+
 var browserSync = require('browser-sync').create();
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var environments = require('gulp-environments');
-var connect = require('gulp-connect');
-var sourcemaps = require('gulp-sourcemaps');
+
+var util = require('gulp-util');
+var gulpWebpack = require('gulp-webpack');
+var webpack = require('webpack');
+var inject = require('gulp-inject');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+var less = require('gulp-less');
 var cssnano = require('gulp-cssnano');
-var clean = require('gulp-clean');
-var livereload = require('gulp-livereload');
+var sourcemaps = require('gulp-sourcemaps');
+var lessAutoprefix = require('less-plugin-autoprefix');
+var hash = require('gulp-hash');
+
+var del = require('del');
+var runSequence = require('run-sequence');
+
+
+var autoprefix = new lessAutoprefix({
+   browsers: ['last 2 versions']
+});
+
+
+var proxyServer = 'localhost:8080';
 
 var folders = {
    distRoot : './../resources/public',
-   targetRoot: './../../../target/classes/public'
+   targetRoot: './../../../target/classes/public',
+   distTmp : './temp',
+   tempDev: './temp/dev',
+   tempProd: './temp/prod'
 };
 
 var sources = {
-   scriptsVendor : './src/assets/lib/*.js',
+   appEntry: './src/app/app.js',
+   lessEntry: './src/styles/master.less',
    scripts: './src/app/**/*.js',
    views : './src/**/*.html',
-   styles : './src/assets/less/*.less',
-   lessMain: './src/assets/less/master.less'
+   styles : './src/styles/**/*.less',
+   cssAssets: './src/assets/lib/**/*.css',
+   assets: './src/assets/**/*'
 };
 
-var dests = {
-   oldFiles: ['./../resources/public/**/**/**/*_old.html', './../resources/public/**/**/**/*_old.js']
-};
+var tempDest = undefined;
 
-gulp.task('scripts', function(){
-   return gulp.src([sources.scriptsVendor])
-       // .pipe(sourcemaps.init())
-       .pipe(concat('vendor.min.js'))
-       .pipe(uglify())
-       // .pipe(sourcemaps.write())
-       .pipe(gulp.dest(folders.distRoot));
+//----- $ npm start
+gulp.task('serve.dev', function(){
+   tempDest = folders.tempDev;
+
+   runSequence(
+       'clean.temp.dev',
+       ['clean.target', 'build.app.dev', 'views', 'build.styles.dev', 'copy.assets.dev'],
+       'build.index',
+       'copy.to.target',
+       'watch',
+       'start-server'
+   );
+
+
 });
 
-gulp.task('lint', function() {
-   return gulp.src('./src/app/**/*.js')
-       .pipe(jshint())
-       .pipe(jshint.reporter('default'));
+gulp.task('start-server', function() {
+    browserSync.init({
+        port: 3000,
+        proxy: proxyServer,
+        injectChanges: true
+    })
 });
 
-gulp.task('browserify', function() {
+//----- $ npm run build
+gulp.task('build.prod', function() {
+   util.log("Bunding files for prod");
+   tempDest = folders.tempProd;
 
-   return gulp.src('./src/app/app.js')
-       // .pipe(sourcemaps.init())
-       .pipe(browserify( {
-          insertGlobals: true,
-          debug: true
+   runSequence(
+       'clean.temp.prod',
+       ['clean.public', 'build.app.prod', 'views', 'build.styles.prod', 'copy.assets.prod'],
+       'build.index',
+       'copy.to.public'
+   );
+});
+
+// -- JS --
+gulp.task('build.app.dev', ['lint'], function() {
+   return gulp.src(sources.appEntry)
+       .pipe(gulpWebpack({
+          output: {
+             filename: 'app.js'
+          }
        }))
-       .pipe(concat('main.js'))
-       .pipe(uglify())
-       // .pipe(sourcemaps.write())
-       .pipe(gulp.dest(folders.distRoot))
-       .pipe(gulp.dest(folders.targetRoot));
+       .pipe(gulp.dest(folders.tempDev))
 });
 
-gulp.task('views', function() {
-   gulp.src([sources.views ])
-       .pipe(gulp.dest(folders.distRoot))
-       .pipe(gulp.dest(folders.targetRoot));
+gulp.task('build.app.prod', ['clean.temp.prod', 'lint'], function(){
+
+   return gulp.src(sources.appEntry)
+       .pipe(gulpWebpack({
+              output: {
+                 filename: 'app-[hash].js'
+              },
+              plugins: [new webpack.optimize.UglifyJsPlugin({
+                 output: {
+                    comments: false
+                 }
+              })]
+           },
+           webpack))
+       .pipe(gulp.dest(folders.tempProd));
 });
 
-gulp.task('clean', function() {
-    return gulp.src(dests.oldFiles)
-   .pipe(clean({force: true}));
+gulp.task('lint', function(){
+   gulp.src(sources.scripts)
+       .pipe(jshint())
+       .pipe(jshint.reporter(stylish))
 });
 
-gulp.task('less', function() {
-   gulp.src(sources.lessMain) //
+
+// -- CSS
+gulp.task('build.styles.dev', function(){
+   return gulp.src(sources.lessEntry)
+       // Uncomment to greatly increase compilation time
        // .pipe(sourcemaps.init())
-       .pipe(less())
-       .pipe(cssnano())
-       .pipe(concat('app.css'))
+       .pipe(less({
+          plugins: [autoprefix]
+       }))
        // .pipe(sourcemaps.write())
-       .pipe(gulp.dest(folders.distRoot))
+       .pipe(gulp.dest(tempDest))
+});
+
+gulp.task('build.styles.prod', function() {
+   return gulp.src(sources.lessEntry)
+       .pipe(hash({
+          hashLength: 20,
+          template: '<%= name %>-<%= hash %>.min<%= ext %>'
+       }))
+       .pipe(less({
+          plugins: [autoprefix]
+       }))
+       .pipe(cssnano())
+       .pipe(gulp.dest(tempDest));
+});
+
+
+
+// -- HTML --
+gulp.task('views', function() {
+   return gulp.src([sources.views ])
+       .pipe(gulp.dest(tempDest));
+});
+
+gulp.task('build.index', function(){
+   var target = gulp.src(tempDest+'/index.html');
+   var sources = gulp.src([tempDest+'/**/*.js', tempDest+'/**/*.css'], {read:false});
+
+   return target.pipe(inject(sources, {relative: true}))
+       .pipe(gulp.dest(tempDest));
+});
+
+
+// -- Copy --
+gulp.task('copy.to.target', function(){
+   return gulp.src(tempDest+'/**/*')
        .pipe(gulp.dest(folders.targetRoot))
-       .pipe(livereload());
 });
 
-gulp.task('browser-sync', ['build'], function() {
-   browserSync.init({
-      server: {
-         baseDir: folders.distRoot,
-         // The key is the url to match
-         // The value is which folder to serve (relative to your current working directory)
-         routes: {
-            "/bower_components": "bower_components",
-            "/node_modules": "node_modules"
-         }
-      },
-      browser:"chrome"
-   });
+gulp.task('copy.to.public', function(){
+   return gulp.src(tempDest+'/**/*')
+       .pipe(gulp.dest(folders.distRoot))
 });
 
-gulp.task('connect', function(){
-   connect.server({
-      root: folders.distRoot,
-      livereload: true
-   });
+
+
+// -- Assets --
+gulp.task('copy.assets.dev', function() {
+   return gulp.src(sources.assets)
+       .pipe(gulp.dest(folders.tempDev+'/assets'))
+
 });
 
-gulp.task('copy-lib', function(){
-   return gulp.src('src/assets/lib/**/*.*')
-       .pipe(gulp.dest(folders.distRoot+'/lib'))
-       .pipe(gulp.dest(folders.targetRoot+'/lib'));
+gulp.task('copy.assets.prod', function() {
+   return gulp.src(sources.assets)
+       .pipe(gulp.dest(folders.tempProd+'/assets'))
+
 });
 
-gulp.task('build',['lint', 'less', 'copy-lib', 'scripts', 'browserify', 'views', 'clean']);
 
 
+// -- Watch & reload --
 gulp.task('watch', function(){
-   livereload.listen();
-   gulp.watch(sources.scripts, ['browserify']);
-   gulp.watch(sources.styles, ['less']);
-   gulp.watch(sources.views, ['views']);
+   gulp.watch(sources.scripts, ['reload.scripts']);
+   gulp.watch(sources.styles, ['reload.less']);
+   gulp.watch(sources.views, ['reload.views']);
+});
+
+gulp.task('reload.scripts', function() {
+   runSequence('build.app.dev', 'build.index', 'copy.to.target', 'reload-browser')
+});
+
+gulp.task('reload.less', function() {
+   runSequence('build.styles.dev', 'build.index', 'copy.to.target', 'reload-browser')
+});
+
+gulp.task('reload.views', function(){
+   runSequence('views', 'build.index', 'copy.to.target', 'reload-browser')
+});
+
+gulp.task('reload-browser', function(done){
+   browserSync.reload();
+   done();
 });
 
 
-gulp.task('watch-dist', function(){
-   gulp.watch(folders.distRoot+"/**/*.*").on('change', function(file) {
-      gulp.src(file.path)
-          .pipe(connect.reload());
-   });
+// -- Clean --
+gulp.task('clean.temp.dev', function(cb){
+   return del(folders.tempDev, cb);
 });
 
-gulp.task('br', ['browser-sync'], function(){
-	gulp.watch("./src/**/*.*", ["build"]);
-	gulp.watch("./../Resources/public/**/*.*").on('change', browserSync.reload);
+gulp.task('clean.temp.prod', function(cb){
+   return del(folders.tempProd, cb);
 });
 
-gulp.task('default', ['build', 'watch', 'watch-dist', 'br']);
+gulp.task('clean.public', function(cb) {
+   return del(folders.distRoot, {force:true}, cb);
+});
 
+gulp.task('clean.target', function(cb) {
+   return del(folders.targetRoot, {force:true}, cb);
+});
 
 
 
