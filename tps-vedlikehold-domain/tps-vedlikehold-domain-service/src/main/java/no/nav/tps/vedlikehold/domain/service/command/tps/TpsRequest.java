c521e8144da9996305259c9ac28269e57f101579
f148888 <peter.sergio.flogstad@nav.no>
2016-11-08 14:56:23 +0100

    Added a way to get parameter value from TpsRequest using the parameter key and not just Get method

--- tps-vedlikehold-domain/tps-vedlikehold-domain-service/src/main/java/no/nav/tps/vedlikehold/domain/service/command/tps/TpsRequest.java
+++ tps-vedlikehold-domain/tps-vedlikehold-domain-service/src/main/java/no/nav/tps/vedlikehold/domain/service/command/tps/TpsRequest.java
@@ -2,12 +2,16 @@ package no.nav.tps.vedlikehold.domain.service.command.tps;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
+import java.util.HashMap;
+
 /**
  * Created by f148888 on 29.09.2016.
  */
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TpsRequest {
+
+    HashMap<String, Object> parameters = new HashMap<>();
     private String environment;
 
     public String getEnvironment() {
@@ -16,6 +20,14 @@ public class TpsRequest {
 
     public void setEnvironment(String environment) {
         this.environment = environment;
+        addToParameterMap("environment", environment);
     }
 
+    protected void addToParameterMap(String paramKey, String paramValue){
+        parameters.put(paramKey, paramValue);
+    }
+
+    public String getParamValue(String paramKey){
+        return parameters.get(paramKey).toString();
+    }
 }
