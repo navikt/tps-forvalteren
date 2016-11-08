e238c71a7a277649a446a4c264131341a7dfb75b
f148888 <peter.sergio.flogstad@nav.no>
2016-11-08 15:10:29 +0100

    Added authorisation before sent request.

--- tps-vedlikehold-service/tps-vedlikehold-service-java/src/main/java/no/nav/tps/vedlikehold/service/command/tps/DefaultTpsRequestService.java
+++ tps-vedlikehold-service/tps-vedlikehold-service-java/src/main/java/no/nav/tps/vedlikehold/service/command/tps/DefaultTpsRequestService.java
@@ -1,12 +1,17 @@
 package no.nav.tps.vedlikehold.service.command.tps;
 
+import no.nav.tps.vedlikehold.common.java.message.MessageProvider;
 import no.nav.tps.vedlikehold.consumer.mq.consumers.MessageQueueConsumer;
 import no.nav.tps.vedlikehold.consumer.mq.factories.MessageQueueServiceFactory;
 import no.nav.tps.vedlikehold.consumer.ws.fasit.config.FasitConstants;
-import no.nav.tps.vedlikehold.domain.service.command.authorisation.User;
+import no.nav.tps.vedlikehold.domain.service.command.User.User;
+import no.nav.tps.vedlikehold.domain.service.command.tps.TpsMessage;
 import no.nav.tps.vedlikehold.domain.service.command.tps.TpsSystemInfo;
 import no.nav.tps.vedlikehold.domain.service.command.tps.ajourforing.requests.TpsRequestEndringsmelding;
 import no.nav.tps.vedlikehold.domain.service.command.tps.servicerutiner.requests.TpsRequestServiceRoutine;
+import no.nav.tps.vedlikehold.provider.rs.security.user.UserContextHolder;
+import no.nav.tps.vedlikehold.service.command.authorisation.TpsAuthorisationService;
+import no.nav.tps.vedlikehold.service.command.exceptions.HttpUnauthorisedException;
 import no.nav.tps.vedlikehold.service.command.tps.utils.TpsRequestXmlCreator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +20,6 @@ import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
 import java.io.IOException;
-
 
 @Service
 public class DefaultTpsRequestService implements TpsRequestService {
@@ -28,6 +32,12 @@ public class DefaultTpsRequestService implements TpsRequestService {
     @Autowired
     private TpsRequestXmlCreator tpsRequestXmlCreator;
 
+    @Autowired
+    private TpsAuthorisationService tpsAuthorisationService;
+
+    @Autowired
+    private MessageProvider messageProvider;
+
     /**
      * Send a request to TPS using asynchronous message queues
      *
@@ -36,7 +46,10 @@ public class DefaultTpsRequestService implements TpsRequestService {
      * @throws JMSException failed to send the message
      */
     @Override
-    public String executeServiceRutineRequest(TpsRequestServiceRoutine tpsRequest) throws IOException, JMSException {
+    public String executeServiceRutineRequest(TpsRequestServiceRoutine tpsRequest, TpsMessage tpsMessage) throws IOException, JMSException, HttpUnauthorisedException {
+        if (!tpsAuthorisationService.userIsAuthorisedToReadPersonInEnvironment(tpsMessage, tpsRequest)) {
+            throw new HttpUnauthorisedException(messageProvider.get("rest.service.request.exception.Unauthorized"), "api/v1/service/" + tpsRequest.getServiceRutinenavn());
+        }
         try {
             MessageQueueConsumer messageQueueConsumer = messageQueueServiceFactory.createMessageQueueService(tpsRequest.getEnvironment(), FasitConstants.REQUEST_QUEUE_SERVICE_RUTINE_ALIAS);
             String xmlRequestMessage = tpsRequestXmlCreator.createXmlTpsRequestServiceRutine(tpsRequest);
