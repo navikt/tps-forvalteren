bc0c6533fac0a0231fa204fc446ae3a8f0453981
f148888 <peter.sergio.flogstad@nav.no>
2016-11-10 07:30:33 +0100

    Added function authorising fodselsnummer and authorsing just the rest call. The read / write privileges

--- tps-vedlikehold-service/tps-vedlikehold-service-java/src/main/java/no/nav/tps/vedlikehold/service/command/authorisation/DefaultTpsAuthorisationService.java
+++ tps-vedlikehold-service/tps-vedlikehold-service-java/src/main/java/no/nav/tps/vedlikehold/service/command/authorisation/DefaultTpsAuthorisationService.java
@@ -1,10 +1,10 @@
 package no.nav.tps.vedlikehold.service.command.authorisation;
 
 import no.nav.tps.vedlikehold.domain.service.command.User.User;
-import no.nav.tps.vedlikehold.domain.service.command.tps.Response;
 import no.nav.tps.vedlikehold.domain.service.command.tps.TpsRequest;
 import no.nav.tps.vedlikehold.domain.service.command.tps.authorisation.strategies.AuthorisationStrategy;
 import no.nav.tps.vedlikehold.domain.service.command.tps.servicerutiner.definition.TpsServiceRoutine;
+import no.nav.tps.vedlikehold.domain.service.command.tps.servicerutiner.requests.TpsHentPersonRequestServiceRoutine;
 import no.nav.tps.vedlikehold.service.command.authorisation.strategy.RestSecurityStrategy;
 import no.nav.tps.vedlikehold.service.command.authorisation.strategy.SearchSecurityStrategy;
 import no.nav.tps.vedlikehold.service.command.authorisation.strategy.SecurityStrategy;
@@ -12,10 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-/**
- * @author Øyvind Grimnes, Visma Consulting AS
- */
 
 @Service
 public class DefaultTpsAuthorisationService implements TpsAuthorisationService {
@@ -26,20 +22,25 @@ public class DefaultTpsAuthorisationService implements TpsAuthorisationService {
     @Autowired
     private List<RestSecurityStrategy> restSecurityStrategies;
 
+    @Autowired
+    private List<SecurityStrategy> securityStrategies;
+
     @Override
     public void authoriseRequest(TpsServiceRoutine serviceRoutine, TpsRequest request, User user) {
-        authorise(searchSecurityStrategies, serviceRoutine, request, user);
+        authorise(securityStrategies, serviceRoutine, request, user);
     }
 
-    public void authoriseRest(TpsServiceRoutine serviceRoutine, TpsRequest request, User user){
+    public void authoriseRestCall(TpsServiceRoutine serviceRoutine, TpsRequest request, User user) {
         authorise(restSecurityStrategies, serviceRoutine, request, user);
     }
 
-    public void authoriseResponse(Response response, TpsServiceRoutine serviceRoutine, TpsRequest request, User user){
-
+    public void authoriseFodselsnummer(TpsServiceRoutine serviceRoutine, String fnr, User user) {
+        TpsHentPersonRequestServiceRoutine singleResultRequest = new TpsHentPersonRequestServiceRoutine();
+        singleResultRequest.setFnr(fnr);
+        authorise(searchSecurityStrategies, serviceRoutine, singleResultRequest, user);
     }
 
-    private void authorise(List<? extends SecurityStrategy> securityStrategies, TpsServiceRoutine serviceRoutine, TpsRequest request, User user){
+    private void authorise(List<? extends SecurityStrategy> securityStrategies, TpsServiceRoutine serviceRoutine, TpsRequest request, User user) {
         for (AuthorisationStrategy authorisationStrategy : serviceRoutine.getSecurityServiceStrategies()) {
             for (SecurityStrategy strategyService : securityStrategies) {
                 if (strategyService.isSupported(authorisationStrategy)) {
@@ -48,7 +49,6 @@ public class DefaultTpsAuthorisationService implements TpsAuthorisationService {
                 }
             }
         }
-
     }
 }
 
