bd1d37807cec1b1e1b23dc189a249762072ec690
f148888 <peter.sergio.flogstad@nav.no>
2017-05-09 13:55:14 +0200

    PK-39363  Fikset endringer foreslått ved QA. Fjernet ubrukt metode og variabler

--- tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/gt/gt.controller.js
+++ tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/gt/gt.controller.js
@@ -2,17 +2,10 @@ angular.module('tps-forvalteren.gt')
     .controller('GTCtrl', ['$scope', '$mdDialog', '$state', 'utilsService', 'environmentsPromise',
         function ($scope, $mdDialog, $state,  utilsService, environmentsPromise) {
 
-            $scope.isServicerutineState = function () {
-                return locationService.isServicerutineState();
-            };
-
             $scope.submit = function () {
                 $scope.formData.aksjonsKode = "B1";
                 var params = utilsService.createParametersFromFormData($scope.formData);
                 serviceRutineFactory.getServiceRutineResponse("FS03-FDNUMMER-KERNINFO-O", params).then(function (res) {
-
-                    var response = res.data.response;
-                    var xml = res.data.xml;
 
                     }, function (error) {
                         console.error(error);
