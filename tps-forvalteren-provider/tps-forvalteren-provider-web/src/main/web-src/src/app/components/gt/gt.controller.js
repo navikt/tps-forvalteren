ecf68da651bba9ebc293e4dd18c7aa63b5f5ac0e
Kristen Hærum <kristen.haerum@nav.no>
2017-05-11 15:22:33 +0200

    PKJAG-5429 Gjort formattering i hht løsningsbeskrivelse; lagt til noe feilhåndtering

--- tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/gt/gt.controller.js
+++ tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/gt/gt.controller.js
@@ -2,11 +2,14 @@ angular.module('tps-forvalteren.gt')
     .controller('GTCtrl', ['$scope', '$mdDialog', '$state', 'utilsService', 'environmentsPromise','serviceRutineFactory',
         function ($scope, $mdDialog, $state,  utilsService, environmentsPromise, serviceRutineFactory) {
 
+            $scope.okStatus = true;
             $scope.submit = function () {
                 $scope.formData.aksjonsKode = "B0";
                 var params = utilsService.createParametersFromFormData($scope.formData);
                 serviceRutineFactory.getServiceRutineResponse("FS03-FDNUMMER-KERNINFO-O", params).then(function (res) {
                         $scope.gt = res.data.response.data[0];
+                        $scope.status = res.data.response.status;
+                        $scope.okStatus = $scope.status.kode == '00'
                     }, function (error) {
                         console.error(error);
                     }
