ff931e8d4eea9c6a15a48364f7eda67465687487
Kristen Hærum <kristen.haerum@nav.no>
2017-05-18 11:58:07 +0200

    PKJAG-5562 Opps flyttet dummy data til feilutgang på tjenestekall

--- tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/testdata/testdata.controller.js
+++ tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/testdata/testdata.controller.js
@@ -9,16 +9,18 @@ angular.module('tps-forvalteren.gt')
             };
 
             var hentTestpersoner = function () {
+                $scope.personer = undefined;
                 testdataService.getTestpersoner().then(
                     function (result) {
                         $scope.personer = result.data;
                     },
                     function (error) {
                         utilsService.showAlertError(error);
+                        // Temp dummy data
+                        $scope.personer = [{ident: "32345678901", identtype: "DNR", fornavn: "Elenora", mellomnavn: "Susan", etternavn: "Dixon", kjonn: "K"},
+                            {ident: "15059023541", identtype: "FNR", fornavn: "Frank", mellomnavn: "Erland", etternavn: "Aaserud", kjonn: "M"}];
                     }
                 );
             }();
 
-            $scope.personer = [{ident: "32345678901", identtype: "DNR", fornavn: "Elenora", mellomnavn: "Susan", etternavn: "Dixon", kjonn: "K"},
-                {ident: "15059023541", identtype: "FNR", fornavn: "Frank", mellomnavn: "Erland", etternavn: "Aaserud", kjonn: "M"}];
         }]);
