169842a8aa96203ffcef4583f7d3c6f4677bd17c
Karadas, Kenneth <Kenneth.Karadas@nav.no>
2017-05-22 10:21:59 +0200

    PK-41256 PKJAG-5592 flyway-script for db

---
+++ tps-forvalteren-config/src/main/resources/db/migration/tpsforvalterenDB/V1.0__Initialize_Tables.sql
@@ -0,0 +1,27 @@
+-----------------
+-- T A B L E S --
+-----------------
+CREATE TABLE T_PERSON (
+  PERSON_ID      NUMBER                 NOT NULL,
+  IDENT          CHAR(11)               NOT NULL,
+  IDENTTYPE      CHAR(3)                NOT NULL,
+  KJONN          CHAR(1)                NOT NULL,
+  FORNAVN        VARCHAR2(50),
+  MELLOMNAVN     VARCHAR2(50),
+  ETTERNAVN      VARCHAR2(50),
+  KORTNAVN       VARCHAR2(25),
+  OPPRETTET_DATO TIMESTAMP(6)           NOT NULL,
+  OPPRETTET_AV   VARCHAR2(20)           NOT NULL,
+  ENDRET_DATO    TIMESTAMP(6)           NOT NULL,
+  ENDRET_AV      VARCHAR2(20)           NOT NULL
+);
+
+---------------------------------------------------
+-- P R I M A R Y   K E Y   C O N S T R A I N T S --
+---------------------------------------------------
+ALTER TABLE T_PERSON     ADD CONSTRAINT T_PERSON_PK     PRIMARY KEY (PERSON_ID);
+
+-----------------------
+-- S E Q U E N C E S --
+-----------------------
+CREATE SEQUENCE T_PERSON_SEQ     START WITH 100000000;
