1b5fc10633e7350428b9b802aea31b87a10d3bae
f148888 <peter.sergio.flogstad@nav.no>
2017-06-06 13:25:23 +0200

    PKJAG-5780 Innføre nye roller. Endret hvilke AD roller TPSF bruker

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/user/UserRole.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/user/UserRole.java
@@ -2,30 +2,26 @@ package no.nav.tps.forvalteren.service.user;
 
 import org.springframework.security.core.GrantedAuthority;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * System user roles
- *
  */
 public enum UserRole implements GrantedAuthority {
 
-    ROLE_ACCESS("0000-GA-NORG_Skriv"),
+    ROLE_ACCESS("0000-GA-TPSF-LES", "0000-GA-TPSF-SKRIV"),
+    ROLE_SKRIV_TPSF("0000-GA-TPSF-SKRIV"),
+    ROLE_LES_TPSF("0000-GA-TPSF-LES"),
     ROLE_DISKRESJONESKODE_6_READ("0000-GA-GOSYS_KODE6"),
     ROLE_DISKRESJONESKODE_7_READ("0000-GA-GOSYS_KODE7"),
     ROLE_EGEN_ANSATT_READ("0000-GA-PIP_EGENANSATT");
 
-    private final String name;
+    private final ArrayList<String> AD_STRINGS = new ArrayList<>();
 
-    UserRole(final String name){
-        this.name = name;
-    }
-
-    public static UserRole getEnumFromName(String roleName){
-        for(UserRole roleEnum : values()){
-            if(roleEnum.toString().equals(roleName)){
-                return roleEnum;
-            }
-        }
-        return null;
+    UserRole(String... name){
+        AD_STRINGS.addAll(Arrays.asList(name));
     }
 
     @Override
@@ -33,8 +29,7 @@ public enum UserRole implements GrantedAuthority {
         return name();
     }
 
-    @Override
-    public String toString(){
-        return name;
+    public List<String> getADRolesForEnum(){
+        return AD_STRINGS;
     }
 }
