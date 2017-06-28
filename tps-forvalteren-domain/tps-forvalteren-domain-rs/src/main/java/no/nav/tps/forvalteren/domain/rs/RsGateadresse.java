a36c1e651100343e55b2227477f3d59484cee22d
Karadas, Kenneth <Kenneth.Karadas@nav.no>
2017-06-28 15:28:26 +0200

    PK-41277 PKJAG-6055 sonarfix

--- tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsGateadresse.java
+++ tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsGateadresse.java
@@ -1,8 +1,17 @@
 package no.nav.tps.forvalteren.domain.rs;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RsGateadresse {
 
     private Long id;
--- tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsPostadresse.java
+++ tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsPostadresse.java
@@ -1,5 +1,14 @@
 package no.nav.tps.forvalteren.domain.rs;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RsPostadresse {
 
     private Long id;
