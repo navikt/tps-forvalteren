22baf7dc25b73178b9849979c52a342572c68eec
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2017-09-27 15:25:28 +0200

    Add beginning of senddoedsmel test

---
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/test/java/no/nav/tps/forvalteren/service/command/testdata/skd/SendDoedsmeldingTest.java
@@ -0,0 +1,59 @@
+package no.nav.tps.forvalteren.service.command.testdata.skd;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.constraints.Max;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import no.nav.tps.forvalteren.domain.jpa.Person;
+import no.nav.tps.forvalteren.service.command.testdata.FindGruppeById;
+import no.nav.tps.forvalteren.service.command.testdata.SaveDoedsmelding;
+import no.nav.tps.forvalteren.service.command.testdata.SjekkDoedsmeldingSentForPersonId;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SendDoedsmeldingTest {
+
+    private Person anAlivePerson;
+    private Person aDeadPerson;
+    private LocalDateTime doedsdato;
+    private List<Person> personer;
+
+    @Mock
+    private SkdCreatePersoner skdCreatePersonerMock;
+
+    @Mock
+    private FindGruppeById findGruppeByIdMock;
+
+    @Mock
+    private SjekkDoedsmeldingSentForPersonId sjekkDoedsmeldingSentForPersonIdMock;
+
+    @Mock
+    private SaveDoedsmelding saveDoedsmeldingMock;
+
+    @InjectMocks
+    private SendDoedsmelding sendDoedsmelding;
+
+    @Before
+    public void setup() {
+        anAlivePerson = new Person();
+        aDeadPerson = new Person();
+
+        doedsdato = LocalDateTime.now();
+
+        aDeadPerson.setDoedsdato(doedsdato);
+
+        personer = new ArrayList<>();
+        personer.add(anAlivePerson);
+        personer.add(aDeadPerson);
+    }
+
+    @Test
+    public void findDoedePersonerTest() {
+    }
+}
