88379382474499659861c70409205940acd6fde8
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2017-10-06 09:37:22 +0200

    PK-44994 PKJAG-6870 PKJAG-6875 Add new test and extend existing

--- tps-forvalteren-repository/tps-forvalteren-repository-component-test/src/test/java/no/nav/tps/forvalteren/repository/jpa/DoedsmeldingRepositoryComponentTest.java
+++ tps-forvalteren-repository/tps-forvalteren-repository-component-test/src/test/java/no/nav/tps/forvalteren/repository/jpa/DoedsmeldingRepositoryComponentTest.java
@@ -1,10 +1,13 @@
 package no.nav.tps.forvalteren.repository.jpa;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,50 +38,78 @@ public class DoedsmeldingRepositoryComponentTest {
     @Autowired
     DoedsmeldingTestRepository doedsmeldingTestRepository;
 
-    private Person aPerson;
-    private Doedsmelding aDoedsmelding;
+    private Person person1;
+    private Person person2;
+    private Doedsmelding doedsmelding1;
+    private Doedsmelding doedsmelding2;
+    private List<Doedsmelding> doedsmeldinger;
 
     @Before
     public void setup() {
-        aPerson = PersonProvider.aMalePerson().build();
-        personRepository.save(Arrays.asList(aPerson));
+        person1 = PersonProvider.aMalePerson().build();
+        person2 = PersonProvider.aFemalePerson().build();
+        personRepository.save(Arrays.asList(person1, person2));
 
-        aDoedsmelding = new Doedsmelding();
-        aDoedsmelding.setPerson(aPerson);
-        aDoedsmelding.setIsMeldingSent(MELDING_SENT);
+        doedsmelding1 = new Doedsmelding();
+        doedsmelding1.setPerson(person1);
+        doedsmelding1.setIsMeldingSent(MELDING_SENT);
+
+        doedsmelding2 = new Doedsmelding();
+        doedsmelding2.setPerson(person2);
+
+        doedsmeldinger = Arrays.asList(doedsmelding1, doedsmelding2);
     }
 
     @Test
     @Rollback
     public void findByPersonIdTest() {
-        doedsmeldingTestRepository.save(aDoedsmelding);
+        doedsmeldingTestRepository.save(doedsmeldinger);
 
-        Doedsmelding result = doedsmeldingRepository.findByPersonId(aPerson.getId());
+        Doedsmelding result = doedsmeldingRepository.findByPersonId(person1.getId());
 
-        assertThat(result, is(aDoedsmelding));
-        assertThat(result.getPerson(), is(aPerson));
+        assertThat(result, is(doedsmelding1));
+        assertThat(result.getPerson(), is(person1));
         assertThat(result.getIsMeldingSent(), is(MELDING_SENT));
+        assertThat(result, not(is(doedsmelding2)));
     }
 
     @Test
     @Rollback
     public void saveTest() {
-        doedsmeldingRepository.save(Arrays.asList(aDoedsmelding));
+        doedsmeldingRepository.save(doedsmeldinger);
 
-        Doedsmelding result = doedsmeldingTestRepository.findAll().iterator().next();
-        assertThat(result, is(aDoedsmelding));
-        assertThat(result.getPerson(), is(aPerson));
-        assertThat(result.getIsMeldingSent(), is(MELDING_SENT));
+        List<Doedsmelding> results = (List<Doedsmelding>) doedsmeldingTestRepository.findAll();
+
+        assertThat(results.size(), is(equalTo(doedsmeldinger.size())));
+        assertThat(results, hasItem(doedsmelding1));
+        assertThat(results, hasItem(doedsmelding2));
     }
 
     @Test
     @Rollback
-    public void deleteByPersonIdInTest() {
-        doedsmeldingTestRepository.save(Arrays.asList(aDoedsmelding));
+    public void deleteOneByPersonIdInTest() {
+        doedsmeldingTestRepository.save(doedsmeldinger);
+        List<Long> idsToDelete = Arrays.asList(person1.getId());
 
-        doedsmeldingRepository.deleteByPersonIdIn(Arrays.asList(aPerson.getId()));
+        doedsmeldingRepository.deleteByPersonIdIn(idsToDelete);
 
-        Doedsmelding result = doedsmeldingTestRepository.findOne(aDoedsmelding.getId());
-        assertThat(result, is(nullValue()));
+        List<Doedsmelding> results = (List<Doedsmelding>) doedsmeldingTestRepository.findAll();
+        assertThat(results.size(), is(equalTo(doedsmeldinger.size() - idsToDelete.size())));
+        assertThat(results, not(hasItem(doedsmelding1)));
+        assertThat(results, hasItem(doedsmelding2));
+    }
+
+    @Test
+    @Rollback
+    public void deleteMultipleByPersonIdInTest() {
+        doedsmeldingTestRepository.save(doedsmeldinger);
+        List<Long> idsToDelete = Arrays.asList(person1.getId(), person2.getId());
+
+        doedsmeldingRepository.deleteByPersonIdIn(idsToDelete);
+
+        List<Doedsmelding> results = (List<Doedsmelding>) doedsmeldingTestRepository.findAll();
+        assertThat(results.size(), is(equalTo(doedsmeldinger.size() - idsToDelete.size())));
+        assertThat(results, not(hasItem(doedsmelding1)));
+        assertThat(results, not(hasItem(doedsmelding2)));
     }
 }
