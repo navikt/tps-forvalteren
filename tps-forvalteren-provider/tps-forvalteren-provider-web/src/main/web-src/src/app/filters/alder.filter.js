4e2920b7ba2cb4b4f0171cc63941cc121b2a48d9
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2017-10-13 16:44:24 +0200

    Improve responsiveness of testdata module

--- tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/filters/alder.filter.js
+++ tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/filters/alder.filter.js
@@ -45,7 +45,7 @@ angular.module('tps-forvalteren.filter')
                 var individ = parseInt(fnr.substring(6, 9));
                 var aarhundre = individ < 500 || individ >= 900 ? '19' : '20';
 
-                return (gregorianAge(new Date(aarhundre + fnr.substring(4, 6), fnr.substring(2, 4) - 1, fnr.substring(0, 2)), doedsdato)) + ' år';
+                return (gregorianAge(new Date(aarhundre + fnr.substring(4, 6), fnr.substring(2, 4) - 1, fnr.substring(0, 2)), doedsdato)) + '\u00A0år';
             }
         };
     });
