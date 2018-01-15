beeb0a9c08aa8ad4c0f21857b536961e1c361206
Karadas, Kenneth <Kenneth.Karadas@nav.no>
2018-01-15 14:40:38 +0100

    PK-44374 PKJAG-7476 PKJAG-7353 Fiksa bug med over 1000 values i query

--- tps-forvalteren-service/tps-forvalteren-service-java/src/test/java/no/nav/tps/forvalteren/service/command/testdata/FindPersonerByIdInTest.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/test/java/no/nav/tps/forvalteren/service/command/testdata/FindPersonerByIdInTest.java
@@ -1,9 +1,13 @@
 package no.nav.tps.forvalteren.service.command.testdata;
 
+import static no.nav.tps.forvalteren.service.command.testdata.utils.TestdataConstants.ORACLE_MAX_IN_SET_ELEMENTS;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -21,13 +25,28 @@ public class FindPersonerByIdInTest {
     @InjectMocks
     private FindPersonerByIdIn findPersonerByIdIn;
 
+    @Mock
+    private List<String> identer;
+
+    @Before
+    public void setup() {
+        when(identer.size()).thenReturn(ORACLE_MAX_IN_SET_ELEMENTS);
+    }
+
     @Test
     public void checkThatRepostioryGetsCalled() {
-        List<String> identer = new ArrayList<>();
-
         findPersonerByIdIn.execute(identer);
 
         verify(personRepository).findByIdentIn(identer);
+    }
+
+    @Test
+    public void checkThatRepostioryGetsCalledMultipleTimes() {
+        when(identer.size()).thenReturn(ORACLE_MAX_IN_SET_ELEMENTS * 10);
+
+        findPersonerByIdIn.execute(identer);
+
+        verify(personRepository, times(10)).findByIdentIn(anyListOf(String.class));
     }
 
 }
