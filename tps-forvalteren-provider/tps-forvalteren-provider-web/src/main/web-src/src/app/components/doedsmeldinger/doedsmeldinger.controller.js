7abf5e77e835a5eada5e76cdb8f4d6e117b179d4
p152410 <kim.pollvik@nav.no>
2018-01-22 09:26:21 +0100

    REG-20 Dødsmeldinger:
    Lagt til inputfeltene til skjermbilde

--- tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/doedsmeldinger/doedsmeldinger.controller.js
+++ tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/doedsmeldinger/doedsmeldinger.controller.js
@@ -1,8 +1,25 @@
 angular.module('tps-forvalteren.doedsmeldinger', ['ngMaterial'])
-    .controller('SendDoedsmeldingerCtrl', ['$scope', '$mdDialog', '$rootScope', '$stateParams', 'endringsmeldingService', 'locationService', 'utilsService', 'headerService',
-        function ($scope, $mdDialog, $rootScope, $stateParams, endringsmeldingService, locationService, utilsService, headerService) {
+    .controller('SendDoedsmeldingerCtrl', ['$scope', '$mdDialog', '$rootScope', '$stateParams', 'locationService', 'utilsService', 'headerService',
+        function ($scope, $mdDialog, $rootScope, $stateParams, locationService, utilsService, headerService) {
 
-            headerService.setHeader('Send Dødsmeldinger');
+            headerService.setHeader('Dødsmelding');
 
+
+
+
+
+
+
+
+            var init = function() {
+                var environments = $scope.$resolve.environmentsPromise;
+                if(environments.status !== undefined){
+                    utilsService.showAlertError(environments);
+                } else {
+                    $scope.environments = utilsService.sortEnvironments(environments.environments);
+                }
+            };
+
+            init();
 
         }]);
