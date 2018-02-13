d6e10c1ad9b07f4e8ffbf6442fe5a547ef70333c
p152410 <kim.pollvik@nav.no>
2018-02-13 11:42:40 +0100

    REG-21 / REG-25 Dødsmeldinger
    Lagt til sjekk på identer i DB og Miljø, ny løsning.

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/SjekkIdenterForDodsmelding.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/SjekkIdenterForDodsmelding.java
@@ -1,24 +1,22 @@
 package no.nav.tps.forvalteren.service.command.testdata;
 
-import no.nav.tps.forvalteren.repository.jpa.DeathRowRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import no.nav.tps.forvalteren.service.command.testdata.opprett.FindIdenterNotUsedInDB;
+
+import no.nav.tps.forvalteren.repository.jpa.DeathRowRepository;
 import no.nav.tps.forvalteren.service.command.testdata.response.IdentMedStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SjekkIdenterForDodsmelding extends SjekkIdenter {
 
     private static final String IKKE_GYLDIG = "IG";
     private static final String FINNES = "FIN";
-    private static final String LEDIG_OG_GYLDIG = "LOG";
-    private static final String FINNES_I_MILJOE = "FIM";
+    private static final String LEDIG_I_MILJO = "LIM";
 
     @Autowired
     private DeathRowRepository deathRowRepository;
@@ -26,17 +24,10 @@ public class SjekkIdenterForDodsmelding extends SjekkIdenter {
     @Autowired
     private SjekkOmGyldigeIdenter sjekkOmGyldigeIdenter;
 
-    @Autowired
-    private FindIdenterNotUsedInDB findIdenterNotUsedInDB;
-
-    @Autowired
-    private FiltrerPaaIdenterTilgjengeligeIMiljo filtrerPaaIdenterTilgjengeligeIMiljo;
 
     public Set<IdentMedStatus> finnGyldigeOgLedigeIdenterForDoedsmeldinger(List<String> identer, String miljo) {
         Set<String> ukjenteIdenter = new HashSet<>(identer);
         Map<String, String> identerMedStatus = new HashMap<>();
-        Map<String, String> identerLedigIMiljoOgDB = new HashMap<>();
-
 
         Set<String> gyldigeIdenter = sjekkOmGyldigeIdenter.execute(ukjenteIdenter);
         setStatusOnDifference(ukjenteIdenter, gyldigeIdenter, identerMedStatus, IKKE_GYLDIG);
@@ -44,31 +35,25 @@ public class SjekkIdenterForDodsmelding extends SjekkIdenter {
         Set<String> ledigeIdenterDBOgMiljo = finnLedigeIdenterDBOgMiljoOgSetStatus(identerMedStatus, gyldigeIdenter, miljo);
         setStatusOnDifference(gyldigeIdenter, ledigeIdenterDBOgMiljo, identerMedStatus, FINNES);
 
-        for (String ident : identerMedStatus.keySet()) {
-
-            if (deathRowRepository.findByIdentAndMiljoe(ident, miljo) == null) {
-                identerLedigIMiljoOgDB.put(ident, miljo);
-            }
-        }
-        if (identerLedigIMiljoOgDB != null){
-            insertIntoMap(identerMedStatus, identerLedigIMiljoOgDB.keySet(), FINNES_I_MILJOE);
-        }
-
-
         System.out.println(identerMedStatus);
         return mapToIdentMedStatusSet(identerMedStatus);
     }
 
     private Set<String> finnLedigeIdenterDBOgMiljoOgSetStatus(Map<String, String> identerMedStatus, Set<String> gyldigeIdenter, String miljoe) {
-        Set<String> ledigeIdenterDB = findIdenterNotUsedInDB.filtrer(gyldigeIdenter);
-        Set<String> environments = new HashSet<>();
-        environments.add(miljoe);
-        Set<String> ledigeIdenterMiljo = filtrerPaaIdenterTilgjengeligeIMiljo.filtrer(gyldigeIdenter, environments);
 
-        Set<String> ledigeIdenterDBOgMiljo = new HashSet<>(ledigeIdenterMiljo);
-        ledigeIdenterDBOgMiljo.retainAll(ledigeIdenterDB);
+        Set<String> ledigeIdenterDBOgMiljo = new HashSet<>();
 
-        insertIntoMap(identerMedStatus, ledigeIdenterDBOgMiljo, LEDIG_OG_GYLDIG);
+
+        for (String ident : gyldigeIdenter) {
+
+            if ( deathRowRepository.findByIdentAndMiljoe(ident, miljoe) == null) {
+                ledigeIdenterDBOgMiljo.add(ident);
+                insertIntoMap(identerMedStatus, ledigeIdenterDBOgMiljo, LEDIG_I_MILJO);
+            } else {
+                insertIntoMap(identerMedStatus, ledigeIdenterDBOgMiljo, FINNES);
+            }
+        }
+
         return ledigeIdenterDBOgMiljo;
     }
 }
