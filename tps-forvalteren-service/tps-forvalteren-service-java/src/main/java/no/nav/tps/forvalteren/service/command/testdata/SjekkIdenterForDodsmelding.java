a3637748e1ce6fd196e62594cfd401fe2eee790f
p152410 <kim.pollvik@nav.no>
2018-02-13 09:49:46 +0100

    REG-21 / REG-25 Dødsmeldinger
    Lagt til sjekk på identer i DB og Miljø

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/SjekkIdenterForDodsmelding.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/SjekkIdenterForDodsmelding.java
@@ -1,5 +1,6 @@
 package no.nav.tps.forvalteren.service.command.testdata;
 
+import no.nav.tps.forvalteren.repository.jpa.DeathRowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,10 @@ public class SjekkIdenterForDodsmelding extends SjekkIdenter {
     private static final String IKKE_GYLDIG = "IG";
     private static final String FINNES = "FIN";
     private static final String LEDIG_OG_GYLDIG = "LOG";
+    private static final String FINNES_I_MILJOE = "FIM";
+
+    @Autowired
+    private DeathRowRepository deathRowRepository;
 
     @Autowired
     private SjekkOmGyldigeIdenter sjekkOmGyldigeIdenter;
@@ -30,12 +35,27 @@ public class SjekkIdenterForDodsmelding extends SjekkIdenter {
     public Set<IdentMedStatus> finnGyldigeOgLedigeIdenterForDoedsmeldinger(List<String> identer, String miljo) {
         Set<String> ukjenteIdenter = new HashSet<>(identer);
         Map<String, String> identerMedStatus = new HashMap<>();
+        Map<String, String> identerLedigIMiljoOgDB = new HashMap<>();
+
 
         Set<String> gyldigeIdenter = sjekkOmGyldigeIdenter.execute(ukjenteIdenter);
         setStatusOnDifference(ukjenteIdenter, gyldigeIdenter, identerMedStatus, IKKE_GYLDIG);
 
         Set<String> ledigeIdenterDBOgMiljo = finnLedigeIdenterDBOgMiljoOgSetStatus(identerMedStatus, gyldigeIdenter, miljo);
         setStatusOnDifference(gyldigeIdenter, ledigeIdenterDBOgMiljo, identerMedStatus, FINNES);
+
+        for (String ident : identerMedStatus.keySet()) {
+
+            if (deathRowRepository.findByIdentAndMiljoe(ident, miljo) == null) {
+                identerLedigIMiljoOgDB.put(ident, miljo);
+            }
+        }
+        if (identerLedigIMiljoOgDB != null){
+            insertIntoMap(identerMedStatus, identerLedigIMiljoOgDB.keySet(), FINNES_I_MILJOE);
+        }
+
+
+        System.out.println(identerMedStatus);
         return mapToIdentMedStatusSet(identerMedStatus);
     }
 
