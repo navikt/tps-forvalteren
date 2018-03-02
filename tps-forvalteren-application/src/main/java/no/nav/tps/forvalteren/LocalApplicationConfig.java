72d589c49df733ca1ad0ada3ba1c775d4ae2fc7d
p152410 <kim.pollvik@nav.no>
2018-03-02 12:36:32 +0100

    REG-67 / PKJAG-8928 - Tpsf på Nais.
    Renamet resources folder, og la til provider-web i application pom.

--- tps-forvalteren-application/src/main/java/no/nav/tps/forvalteren/LocalApplicationConfig.java
+++ tps-forvalteren-application/src/main/java/no/nav/tps/forvalteren/LocalApplicationConfig.java
@@ -4,11 +4,9 @@ import no.nav.tps.forvalteren.common.java.config.CommonConfig;
 import no.nav.tps.forvalteren.common.java.mapping.MapperConfig;
 import no.nav.tps.forvalteren.provider.rs.config.RestProviderConfig;
 import no.nav.tps.forvalteren.repository.jpa.config.RepositoryConfig;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
 
 @Configuration
 @EnableAutoConfiguration
