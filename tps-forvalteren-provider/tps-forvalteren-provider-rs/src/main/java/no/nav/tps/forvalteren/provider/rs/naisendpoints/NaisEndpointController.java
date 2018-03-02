faf48f914cb048675568e21f5594686ef20f55d6
Peter Flogstad <peter.sergio.flogstad@nav.no>
2018-03-02 16:02:24 +0100

    Endret endepunkt internt.

--- tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/naisendpoints/NaisEndpointController.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/naisendpoints/NaisEndpointController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("actuator")
+@RequestMapping("/actuator")
 public class NaisEndpointController {
 
     @RequestMapping(value = "/isAlive", method = RequestMethod.GET)
