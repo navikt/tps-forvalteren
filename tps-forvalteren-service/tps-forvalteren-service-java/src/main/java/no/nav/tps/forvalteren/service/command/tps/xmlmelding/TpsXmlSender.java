540c6a1bb6ea4b90d4c43f612d718eba74cd9444
p152410 <kim.pollvik@nav.no>
2018-04-04 13:08:32 +0200

    PKJAG-9285 Endringsmelding 32 trans får feilmelding.
    * Lagt til sjekk av innsendte melding for å bestemme meldingstype i TpsXmlSender.
    * Redigert test for TpsXmlSender.
    * Lagt til test for ContainsXmlElement-klasse.

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/tps/xmlmelding/TpsXmlSender.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/tps/xmlmelding/TpsXmlSender.java
@@ -1,10 +1,12 @@
 package no.nav.tps.forvalteren.service.command.tps.xmlmelding;
 
+import javax.jms.JMSException;
+
 import no.nav.tps.forvalteren.consumer.mq.consumers.MessageQueueConsumer;
 import no.nav.tps.forvalteren.consumer.mq.factories.MessageFixedQueueServiceFactory;
 import no.nav.tps.forvalteren.domain.rs.RsXmlMelding;
-
-import javax.jms.JMSException;
+import no.nav.tps.forvalteren.service.command.testdata.skd.SkdAddHeaderToSkdMelding;
+import no.nav.tps.forvalteren.service.command.testdata.utils.ContainsXmlElements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,15 @@ public class TpsXmlSender {
     @Autowired
     private MessageFixedQueueServiceFactory messageFixedQueueServiceFactory;
 
+    @Autowired
+    private SkdAddHeaderToSkdMelding skdAddHeaderToSkdMelding;
+
+    @Autowired
+    private ContainsXmlElements containsXmlElements;
+
     public String sendXml(RsXmlMelding rsXmlMelding) throws JMSException {
+
+        addHeaderIfSkdMelding(rsXmlMelding);
 
         MessageQueueConsumer messageQueueConsumer = messageFixedQueueServiceFactory.createMessageQueueConsumerWithFixedQueueName(getEnvironmentFromQueueName(rsXmlMelding.getKo()), rsXmlMelding.getKo());
 
@@ -26,4 +36,13 @@ public class TpsXmlSender {
         return ko.substring(3, ko.indexOf('_'));
     }
 
+    private void addHeaderIfSkdMelding(RsXmlMelding rsXmlMelding) {
+
+        if (!containsXmlElements.execute(rsXmlMelding.getMelding())) {
+            StringBuilder skdMelding = new StringBuilder(rsXmlMelding.getMelding());
+
+            StringBuilder rsXmlMeldingMedHeader = skdAddHeaderToSkdMelding.execute(skdMelding);
+            rsXmlMelding.setMelding(rsXmlMeldingMedHeader.toString());
+        }
+    }
 }
--- tps-forvalteren-service/tps-forvalteren-service-java/src/test/java/no/nav/tps/forvalteren/service/command/tps/TpsXmlSenderTest.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/test/java/no/nav/tps/forvalteren/service/command/tps/TpsXmlSenderTest.java
@@ -3,11 +3,15 @@ package no.nav.tps.forvalteren.service.command.tps;
 import no.nav.tps.forvalteren.consumer.mq.consumers.MessageQueueConsumer;
 import no.nav.tps.forvalteren.consumer.mq.factories.MessageFixedQueueServiceFactory;
 import no.nav.tps.forvalteren.domain.rs.RsXmlMelding;
+import no.nav.tps.forvalteren.service.command.testdata.skd.SkdAddHeaderToSkdMelding;
+import no.nav.tps.forvalteren.service.command.testdata.utils.ContainsXmlElements;
 import no.nav.tps.forvalteren.service.command.tps.xmlmelding.TpsXmlSender;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -18,20 +22,29 @@ public class TpsXmlSenderTest {
 
     private String melding = "test_melding";
     private String ko = "QA.D8_411.TPS_FORESPORSEL_XML_O";
-
+    private StringBuilder rsXmlMeldingMedHeader = new StringBuilder("header " + melding);
     private RsXmlMelding rsXmlMelding = new RsXmlMelding(melding, ko);
 
     @Mock
     private MessageFixedQueueServiceFactory messageFixedQueueServiceFactory;
 
     @Mock
+    private SkdAddHeaderToSkdMelding skdAddHeaderToSkdMelding;
+
+    @Mock
     private MessageQueueConsumer messageQueueConsumer;
+
+    @Mock
+    private ContainsXmlElements containsXmlElements;
 
     @InjectMocks
     private TpsXmlSender tpsXmlSender;
 
     @Before
     public void setup() throws Exception {
+
+        when(containsXmlElements.execute(anyString())).thenReturn(false);
+        when(skdAddHeaderToSkdMelding.execute(anyObject())).thenReturn(rsXmlMeldingMedHeader);
         when(messageFixedQueueServiceFactory.createMessageQueueConsumerWithFixedQueueName("D8", ko))
                 .thenReturn(messageQueueConsumer);
         when(messageQueueConsumer.sendMessage(rsXmlMelding.getMelding())).thenReturn("");
