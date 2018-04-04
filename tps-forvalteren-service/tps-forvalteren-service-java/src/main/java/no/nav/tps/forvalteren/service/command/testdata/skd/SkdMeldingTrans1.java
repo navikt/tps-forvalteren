463aa1ad352f65822e58133561f7274688736a98
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-04-04 12:22:17 +0200

    PKJAG-9010: Opprettet javarepresentasjon av SkdMeldingTrans1.

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/skd/SkdMeldingTrans1.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/skd/SkdMeldingTrans1.java
@@ -1,5 +1,6 @@
 package no.nav.tps.forvalteren.service.command.testdata.skd;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class SkdMeldingTrans1 {
 	private String fodselsdato;
