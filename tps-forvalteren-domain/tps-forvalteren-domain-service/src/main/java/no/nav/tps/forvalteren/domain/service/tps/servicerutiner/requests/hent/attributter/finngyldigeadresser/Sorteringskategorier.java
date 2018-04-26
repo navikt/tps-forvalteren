d31457178fdb55c2d6a25e49d7909d656e67afa6
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-04-26 17:03:38 +0200

    REG-340: Laget sorteringskategorier enum

---
+++ tps-forvalteren-domain/tps-forvalteren-domain-service/src/main/java/no/nav/tps/forvalteren/domain/service/tps/servicerutiner/requests/hent/attributter/finngyldigeadresser/Sorteringskategorier.java
@@ -0,0 +1,5 @@
+package no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.attributter.finngyldigeadresser;
+
+public enum Sorteringskategorier {
+    K,P,N
+}
--- tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/request/param/FinnGyldigeAdresserRequestParam.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/request/param/FinnGyldigeAdresserRequestParam.java
@@ -1,13 +1,11 @@
-package no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent;
+package no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.request.param;
 
-import com.fasterxml.jackson.xml.annotate.JacksonXmlRootElement;
-
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.TpsServiceRoutineHentRequest;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.attributter.finngyldigeadresser.JaEllerNei;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.attributter.finngyldigeadresser.Sorteringskategorier;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.attributter.finngyldigeadresser.Typesok;
@@ -17,16 +15,22 @@ import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.at
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@JacksonXmlRootElement(localName = "tpsServiceRutine")
-public class TpsFinnGyldigeAdresserRequest extends TpsServiceRoutineHentRequest {
+public class FinnGyldigeAdresserRequestParam {
+    
     private String adresseNavnsok;
+    @ApiModelProperty(value = "Tjenesten kan returnere fonetiske likheter(F), eksakte likheter(E) eller tilfeldige likheter(T) med søket.")
     private Typesok typesok;
     private String husNrsok;
     private String kommuneNrsok;
     private String postNrsok;
     private Integer maxRetur;
-    private JaEllerNei alltidRetur;
     private JaEllerNei alleSkrivevarianter;
+    @ApiModelProperty(value = "Ønsker du at postnummer og poststed returneres? J = Ja, N = Nei. Svarer du Nei, får du betydelig raskere responstid.")
     private JaEllerNei visPostnr;
+    @ApiModelProperty(value = "I responsen sorteres gyldige adresser:\n"
+            + " K - Ønskes sortert på kommunenr og deretter adressenavn,\n"
+            + " P - Ønskes sortert på postnr og deretter adressenavn,\n"
+            + " N - Ønskes sortert kun på Adressenavn,\n"
+            + " Blank (ingenting er oppgitt) = N.")
     private Sorteringskategorier sortering;
 }
