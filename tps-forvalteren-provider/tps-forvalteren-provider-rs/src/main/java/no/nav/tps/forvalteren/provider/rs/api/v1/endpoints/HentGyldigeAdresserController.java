d09218651945bf40a337d75a0c4269f9a8baaea4
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-04-26 17:08:44 +0200

    REG-340: La til mapper i autocomplete finnGyldigeAdresser REST-endepunktet og swagger-dokumentasjon i RestController

--- tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/HentGyldigeAdresserController.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/HentGyldigeAdresserController.java
@@ -1,5 +1,9 @@
 package no.nav.tps.forvalteren.provider.rs.api.v1.endpoints;
 
+import static no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.attributter.finngyldigeadresser.JaEllerNei.J;
+import static no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.attributter.finngyldigeadresser.JaEllerNei.N;
+import static no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.attributter.finngyldigeadresser.Typesok.F;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.hent.TpsFinnGyldigeAdresserRequest;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.response.TpsServiceRoutineResponse;
+import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.mapping.MapFinnGyldigeAdresserToTpsServiceRutine;
+import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.request.param.FinnGyldigeAdresserRequestParam;
 import no.nav.tps.forvalteren.service.command.tps.servicerutiner.HentGyldigeAdresserService;
 
 /**
@@ -18,14 +25,14 @@ import no.nav.tps.forvalteren.service.command.tps.servicerutiner.HentGyldigeAdre
 @RequestMapping(value = "api/v1/gyldigadresse")
 public class HentGyldigeAdresserController {
     @Autowired
-    HentGyldigeAdresserService hentGyldigeAdresserService;
+    private HentGyldigeAdresserService hentGyldigeAdresserService;
+    @Autowired
+    private MapFinnGyldigeAdresserToTpsServiceRutine finnGyldigeAdresserMapper;
     
-    /**
-     * Denne tjenesten returnerer en tilfeldig adresse. Default returnerer den én adresse.
-     * Dersom man ønsker flere, så må man oppgi antall i query.
-     * Man kan også legge ved kriterier/spesifikasjoner i spørringen, på hva adressen må inneholde:
-     * kommunenummer, gatenavn (minst 3 tegn), postnummer.
-     */
+    @ApiOperation(value = "Få tilfeldige, gyldige adresser", notes = "Denne tjenesten returnerer en tilfeldig, gyldig adresse. Default returnerer den én adresse.\n"
+            + "     * Dersom man ønsker flere, så må man oppgi antall i query.\n\r"
+            + "     * Man kan også legge ved kriterier/spesifikasjoner i spørringen, på hva adressen må inneholde:\n\r"
+            + "     * kommunenummer, gatenavn (minst 3 tegn), postnummer.")
     @GetMapping("/tilfeldig")
     public TpsServiceRoutineResponse hentTilfeldigAdresse(@RequestParam(required = false, defaultValue = "1") Integer antall,
             @RequestParam(required = false) String kommuneNr,
@@ -33,9 +40,15 @@ public class HentGyldigeAdresserController {
         return hentGyldigeAdresserService.hentTilfeldigAdresse(antall, kommuneNr, postNr);
     }
     
+    @ApiOperation(value = "Søk etter gyldige adresser i TPS", notes = "De query parametrene som ikke spesifiseres, følger default verdier. \n\r"
+            + "Default verdier: typesok(F), visPostnr(J),alleSkrivevarianter(N),maxRetur(5),sortering(N). "
+            + "Gyldige adresser er default sortert på adressenavn(N). Typesøket er Fonetisk(F).")
     @GetMapping("/autocomplete")
     public TpsServiceRoutineResponse finnGyldigAdresse(
-            @ModelAttribute TpsFinnGyldigeAdresserRequest tpsServiceRoutineRequest) { //TODO requestparam klasse, tpservice-mapper og tps-servicerequest. Test denne rest-klassen (med typesok T)
+            @ModelAttribute FinnGyldigeAdresserRequestParam finnGyldigeAdresserRequestParam) {
+        TpsFinnGyldigeAdresserRequest tpsServiceRoutineRequest = TpsFinnGyldigeAdresserRequest.builder().typesok(F).visPostnr(J).alleSkrivevarianter(N).alltidRetur(J).maxRetur(5).build();
+        tpsServiceRoutineRequest = finnGyldigeAdresserMapper.map(finnGyldigeAdresserRequestParam, tpsServiceRoutineRequest);
+        
         return hentGyldigeAdresserService.finnGyldigAdresse(tpsServiceRoutineRequest);
     }
     
