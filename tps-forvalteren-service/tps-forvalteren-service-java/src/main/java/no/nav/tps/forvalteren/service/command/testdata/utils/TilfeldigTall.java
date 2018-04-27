4e0eeb5ed9648d5409c4f2057aa1d42a8362d551
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-04-27 16:39:39 +0200

    REG-340: Sonar rapporterte critical issue på bruk av java.util.Random(). Byttet til ThreadLocalRandom

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/utils/TilfeldigTall.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/utils/TilfeldigTall.java
@@ -1,9 +1,9 @@
 package no.nav.tps.forvalteren.service.command.testdata.utils;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TilfeldigTall {
     public static String  tilfeldigTall(String min, String max) {
-        return String.format("%03d",  new Random().nextInt(Integer.parseInt(min)) + Integer.parseInt(max));
+        return String.format("%03d", ThreadLocalRandom.current().nextInt( Integer.parseInt(min), Integer.parseInt(max)));
     }
 }
