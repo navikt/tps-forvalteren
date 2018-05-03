4ab8c2daa844e62ce97f2c58da1d49d28311f9f5
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-05-03 13:24:51 +0200

    REG-384: Happypath test i CreatePersonerFraIdentlisteTestdataControllerCompTest

---
+++ tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/comptest/testdatacontroller/AbstractTestdataControllerComponentTest.java
@@ -0,0 +1,59 @@
+package no.nav.tps.forvalteren.provider.rs.api.v1.comptest.testdatacontroller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import com.google.common.base.Charsets;
+
+import no.nav.tps.forvalteren.provider.rs.AbstractRsProviderIntegrationTest;
+import no.nav.tps.forvalteren.repository.jpa.GruppeRepository;
+import no.nav.tps.forvalteren.repository.jpa.PersonRepository;
+@Transactional
+public abstract class AbstractTestdataControllerComponentTest extends AbstractRsProviderIntegrationTest{
+    private static final String BASE_URL = "/api/v1/testdata/";
+    private List<NameValuePair> params = new ArrayList<>();
+    @Autowired(required = false)
+    protected WebApplicationContext context;
+    protected MockMvc mvc;
+    @Autowired
+    protected GruppeRepository gruppeRepository;
+    @Autowired
+    protected PersonRepository personRepository;
+    
+    
+    protected void addRequestParam(String key, Object val) {
+        params.add(new BasicNameValuePair(key, val.toString()));
+    }
+    
+    protected abstract String getServiceUrl();
+    
+    protected String getUrl() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BASE_URL);
+        sb.append(getServiceUrl());
+        if (!params.isEmpty()) {
+            sb.append("?");
+            sb.append(URLEncodedUtils.format(params, Charsets.UTF_8));
+        }
+        return sb.toString();
+    }
+    @Before
+    public void setup() {
+        if (context != null) {
+            mvc = MockMvcBuilders.webAppContextSetup(context).build();
+        }
+    }
+    @After
+    public void clearParam() {
+        params.clear();
+    }
+}
---
+++ tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/comptest/testdatacontroller/CreatePersonerFraIdentlisteTestdataControllerCompTest.java
@@ -0,0 +1,53 @@
+package no.nav.tps.forvalteren.provider.rs.api.v1.comptest.testdatacontroller;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+
+import no.nav.tps.forvalteren.domain.jpa.Gruppe;
+import no.nav.tps.forvalteren.domain.jpa.Person;
+import no.nav.tps.forvalteren.provider.rs.api.v1.config.TestUserDetails;
+
+//TODO Opprett H2. Testen kjører mot live Oracle DB i u6. constraint unique navn gjør at gruppenavnet ikke kan eksistere fra før. Og alle identene må også være unike til enhver tid når man kjører testen.
+public class CreatePersonerFraIdentlisteTestdataControllerCompTest extends AbstractTestdataControllerComponentTest {
+
+        private final int IDENT1 = 03051750127;
+        private final int IDENT2 = 02304040404;
+        private Long gruppeId;
+        private Gruppe testgruppe;
+        
+        @Override
+        protected String getServiceUrl() {
+            return "createpersoner/" + gruppeId;
+        }
+        @Before
+        public void setupDatabase(){
+            
+            testgruppe= gruppeRepository.save(Gruppe.builder().navn("regresjonstestgruppenavn").build());
+                    gruppeId = testgruppe.getId();
+            System.out.println(gruppeId);
+        }
+        
+        /**
+         * HVIS en liste med fødselsnummer/identer som er ledige og gyldige, sendes til createpersoner, SÅ skal det opprettes testpersoner i TPSF DB med disse identene.
+         */
+        @Test
+        @WithUserDetails(TestUserDetails.USERNAME)
+        public void shouldCreatePerson() throws Exception {
+            mvc.perform(post(getUrl()).contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content("[\""+IDENT1+"\",\""+IDENT2+"\"]"))
+                    .andExpect(status().isOk());
+            //sjekk db
+            List<Person> persistertePersoner=personRepository.findByIdentIn(Arrays.asList(""+IDENT1, ""+IDENT2));
+            assertEquals(2, persistertePersoner.size());
+            persistertePersoner.forEach(person -> assertEquals(gruppeId, person.getGruppe().getId()));
+        }
+    }
+
