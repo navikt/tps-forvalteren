c9d49ab6a4062c90a2d5b1e06557acb4b4e812ad
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-05-16 10:05:54 +0200

    REG-289: sonar korreksjon

--- tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/KodeverkController.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/KodeverkController.java
@@ -1,15 +1,14 @@
 package no.nav.tps.forvalteren.provider.rs.api.v1.endpoints;
 
-import no.nav.tps.forvalteren.domain.ws.kodeverk.Kode;
-import no.nav.tps.forvalteren.service.kodeverk.KodeverkCache;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import no.nav.tps.forvalteren.domain.ws.kodeverk.Kode;
+import no.nav.tps.forvalteren.service.kodeverk.KodeverkCache;
 
 @RestController
 @RequestMapping(value = "api/v1/kodeverk")
