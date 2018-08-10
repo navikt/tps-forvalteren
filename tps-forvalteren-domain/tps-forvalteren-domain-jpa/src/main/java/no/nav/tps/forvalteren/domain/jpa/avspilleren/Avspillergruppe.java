1b9e91d8f474a050c125da153cdd4221b85dca7c
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-08-10 14:54:01 +0200

    PK-54944, REG-712: JPA for skdmelding-avspilleren + korrigering av flyway-skriptet (la til T_avspillergruppe)

---
+++ tps-forvalteren-domain/tps-forvalteren-domain-jpa/src/main/java/no/nav/tps/forvalteren/domain/jpa/avspilleren/Avspillergruppe.java
@@ -0,0 +1,50 @@
+package no.nav.tps.forvalteren.domain.jpa.avspilleren;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import no.nav.tps.forvalteren.domain.jpa.embedded.ChangeStamp;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "T_AVSPILLERGRUPPE")
+public class Avspillergruppe extends ChangeStamp {
+    
+    private static final String SEQ = "T_GRUPPE_SEQ";
+    
+    @Id
+    @SequenceGenerator(name = SEQ, sequenceName = SEQ, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ)
+    @Column(name = "GRUPPE_ID", nullable = false, updatable = false)
+    private Long id;
+    
+    @Column(name = "NAVN", unique = true, nullable = false, length = 50)
+    private String navn;
+    
+    @Column(name = "BESKRIVELSE", length = 200)
+    private String beskrivelse;
+    
+    @OrderBy("sekvensnummer")
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "avspillergruppe")
+    private List<SkdmeldingAvspillerdata> skdmeldinger = new ArrayList<>();
+}
---
+++ tps-forvalteren-domain/tps-forvalteren-domain-jpa/src/main/java/no/nav/tps/forvalteren/domain/jpa/avspilleren/SkdmeldingAvspillerdata.java
@@ -0,0 +1,52 @@
+package no.nav.tps.forvalteren.domain.jpa.avspilleren;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "T_SKD_MELDINGER_AVSPILLERDATA")
+public class SkdmeldingAvspillerdata {
+    
+    private static final String SEQ = "T_SKD_AVSPILLERDATA_SEQ";
+    
+    @Id
+    @SequenceGenerator(name = SEQ, sequenceName = SEQ, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ)
+    @Column(name = "ID", nullable = false, updatable = false)
+    private Long id;
+    
+    @Column(name = "SKD_MELDING", nullable = false, length = 1600)
+    private String Skdmelding;
+    
+    @Column(name = "TESTPERSON_ID")
+    private int testpersonAvspillerId;
+    
+    @JoinColumn(name = "GRUPPE_ID", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Avspillergruppe avspillergruppe;
+    
+    @Column(nullable = false)
+    private Long sekvensnummer;
+    
+    @Column(nullable = false)
+    private int aarsakskode;
+}
