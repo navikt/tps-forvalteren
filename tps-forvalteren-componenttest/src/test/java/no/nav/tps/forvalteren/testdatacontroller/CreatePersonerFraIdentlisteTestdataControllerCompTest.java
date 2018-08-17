5717b3744aad95beee1f56b8376eeeb5a85f8f99
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-08-17 10:27:46 +0200

    REG-384: Navnekollisjon mellom comptestenes gruppenavn.

--- tps-forvalteren-componenttest/src/test/java/no/nav/tps/forvalteren/testdatacontroller/CreatePersonerFraIdentlisteTestdataControllerCompTest.java
+++ tps-forvalteren-componenttest/src/test/java/no/nav/tps/forvalteren/testdatacontroller/CreatePersonerFraIdentlisteTestdataControllerCompTest.java
@@ -17,36 +17,36 @@ import no.nav.tps.forvalteren.domain.jpa.Person;
 
 @Transactional
 public class CreatePersonerFraIdentlisteTestdataControllerCompTest extends AbstractTestdataControllerComponentTest {
-
-        private final int IDENT1 = 03051750127;
-        private final int IDENT2 = 02304040404;
-        private Long gruppeId;
-        private Gruppe testgruppe;
-        
-        @Override
-        protected String getServiceUrl() {
-            return "/createpersoner/" + gruppeId;
-        }
-        
-        public void setupTestdataInTpsfDatabase(){
-            testgruppe= gruppeRepository.save(Gruppe.builder().navn("regresjonstestgruppenavn").build());
-                    gruppeId = testgruppe.getId();
-        }
-        
-        /**
-         * HVIS en liste med fødselsnummer/identer som er ledige og gyldige, sendes til createpersoner, SÅ skal det opprettes testpersoner i TPSF DB med disse identene.
-         */
-        @Test
-        @WithUserDetails(TestUserDetails.USERNAME)
-        public void shouldCreatePerson() throws Exception {
-            setupTestdataInTpsfDatabase();
-            mvc.perform(post(getUrl()).contentType(MediaType.APPLICATION_JSON_UTF8)
-                    .content("[\""+IDENT1+"\",\""+IDENT2+"\"]"))
-                    .andExpect(status().isOk());
-            //sjekk db
-            List<Person> persistertePersoner=personRepository.findByIdentIn(Arrays.asList(""+IDENT1, ""+IDENT2));
-            assertEquals(2, persistertePersoner.size());
-            persistertePersoner.forEach(person -> assertEquals(gruppeId, person.getGruppe().getId()));
-        }
+    
+    private final int IDENT1 = 03051750127;
+    private final int IDENT2 = 02304040404;
+    private Long gruppeId;
+    private Gruppe testgruppe;
+    
+    @Override
+    protected String getServiceUrl() {
+        return "/createpersoner/" + gruppeId;
     }
+    
+    public void setupTestdataInTpsfDatabase() {
+        testgruppe = gruppeRepository.save(Gruppe.builder().navn("regresjonstestgruppenavn").build());
+        gruppeId = testgruppe.getId();
+    }
+    
+    /**
+     * HVIS en liste med fødselsnummer/identer som er ledige og gyldige, sendes til createpersoner, SÅ skal det opprettes testpersoner i TPSF DB med disse identene.
+     */
+    @Test
+    @WithUserDetails(TestUserDetails.USERNAME)
+    public void shouldCreatePerson() throws Exception {
+        setupTestdataInTpsfDatabase();
+        mvc.perform(post(getUrl()).contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content("[\"" + IDENT1 + "\",\"" + IDENT2 + "\"]"))
+                .andExpect(status().isOk());
+        //sjekk db
+        List<Person> persistertePersoner = personRepository.findByIdentIn(Arrays.asList("" + IDENT1, "" + IDENT2));
+        assertEquals(2, persistertePersoner.size());
+        persistertePersoner.forEach(person -> assertEquals(gruppeId, person.getGruppe().getId()));
+    }
+}
 
