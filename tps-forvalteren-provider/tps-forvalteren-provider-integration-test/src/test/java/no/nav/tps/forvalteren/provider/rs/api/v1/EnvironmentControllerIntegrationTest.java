a236f0370ceb712e17f401086f5fab9e73582125
Kristen Hærum <kristen.haerum@nav.no>
2018-08-31 14:33:29 +0200

    REG-974 Oppdatert environment test

--- tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/EnvironmentControllerIntegrationTest.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/EnvironmentControllerIntegrationTest.java
@@ -1,12 +1,5 @@
 package no.nav.tps.forvalteren.provider.rs.api.v1;
 
-import no.nav.tps.forvalteren.provider.rs.AbstractRsProviderIntegrationTest;
-import no.nav.tps.forvalteren.provider.rs.api.v1.config.TestUserDetails;
-
-import org.junit.Test;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
-
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
@@ -14,6 +7,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+
+import no.nav.tps.forvalteren.provider.rs.AbstractRsProviderIntegrationTest;
+import no.nav.tps.forvalteren.provider.rs.api.v1.config.TestUserDetails;
 
 public class EnvironmentControllerIntegrationTest extends AbstractRsProviderIntegrationTest {
 
@@ -33,7 +33,7 @@ public class EnvironmentControllerIntegrationTest extends AbstractRsProviderInte
                 .andExpect(jsonPath("$.environments", containsInAnyOrder(
                         "u5","u6", "t0","t1", "t2","t3","t4","t5", "t6", "t7","t8","t9","t10","t11","t12", "t13"
                 )))
-                .andExpect(jsonPath("$.productionMode", is(false)));
+                .andExpect(jsonPath("$.productionMode", is(true)));
     }
 
 }
