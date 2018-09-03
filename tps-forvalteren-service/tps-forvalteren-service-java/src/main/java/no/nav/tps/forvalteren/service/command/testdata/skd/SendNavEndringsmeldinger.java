31dd7bf39c84d8d3c018da935b53e0adefc03646
Kristen Hærum <kristen.haerum@nav.no>
2018-09-03 12:15:52 +0200

    REG-973 Lagt til delay i TestController

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/skd/SendNavEndringsmeldinger.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/skd/SendNavEndringsmeldinger.java
@@ -5,6 +5,8 @@ import static no.nav.tps.forvalteren.service.command.testdata.utils.BehandleTpsR
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,48 +23,63 @@ import no.nav.tps.forvalteren.service.user.UserContextHolder;
 
 @Service
 public class SendNavEndringsmeldinger {
-    
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SendNavEndringsmeldinger.class);
+    private static final String DELAY_TIMEOUT_ERROR = "Pacemaker delay error";
+    private static final long MAX_SIZE_WITHOUT_DELAY = 1000L;
+    private static final long DEFAULT_DELAY = 100L;
+
     @Autowired
     private OpprettEgenAnsattMelding opprettEgenAnsattMelding;
-    
+
     @Autowired
     private OpprettSikkerhetstiltakMelding opprettSikkerhetstiltakMelding;
-    
+
     @Autowired
     private TpsRequestSender tpsRequestSender;
-    
+
     @Autowired
     private UserContextHolder userContextHolder;
-    
+
     public List<ServiceRoutineResponseStatus> execute(List<Person> listeMedPersoner, Set<String> environmentsSet) {
-        List<TpsNavEndringsMelding> navEndringsMeldinger = new ArrayList<>();
-        
+
         TpsRequestContext tpsRequestContext = new TpsRequestContext();
         tpsRequestContext.setUser(userContextHolder.getUser());
-    
+
+        List<TpsNavEndringsMelding> navEndringsMeldinger = new ArrayList<>();
         listeMedPersoner.forEach(person -> {
             navEndringsMeldinger.addAll(opprettEgenAnsattMelding.execute(person, environmentsSet));
             navEndringsMeldinger.addAll(opprettSikkerhetstiltakMelding.execute(person, environmentsSet));
         });
-        
+
         List<ServiceRoutineResponseStatus> responseStatuses = new ArrayList<>();
-        navEndringsMeldinger.forEach(serviceRoutineRequest -> {
+        for (int i = 0; i < navEndringsMeldinger.size(); i++) {
+            TpsNavEndringsMelding serviceRoutineRequest = navEndringsMeldinger.get(i);
+
             tpsRequestContext.setEnvironment(serviceRoutineRequest.getMiljo());
             TpsServiceRoutineResponse svar = tpsRequestSender.sendTpsRequest(serviceRoutineRequest.getMelding(), tpsRequestContext);
-            
+
             ResponseStatus status = ekstraherStatusFraServicerutineRespons(svar);
             if ("00" != status.getKode()) {
                 responseStatuses.add(byggRespons(serviceRoutineRequest, status));
             }
-        });
-        
-        if (responseStatuses.isEmpty()) {
-            responseStatuses.add(new ServiceRoutineResponseStatus("Alle identer", "Alle NAV endringsmeldinger", "alle miljøer", new ResponseStatus("OK","","") ));
+            if (i > MAX_SIZE_WITHOUT_DELAY) {
+                try {
+                    Thread.sleep(DEFAULT_DELAY);
+                } catch (InterruptedException e) {
+                    LOGGER.error(String.format("%s: %s", DELAY_TIMEOUT_ERROR, e.getMessage()), e);
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
-        
+
+        if (responseStatuses.isEmpty()) {
+            responseStatuses.add(new ServiceRoutineResponseStatus("Alle identer", "Alle NAV endringsmeldinger", "alle miljøer", new ResponseStatus("OK", "", "")));
+        }
+
         return responseStatuses;
     }
-    
+
     private ServiceRoutineResponseStatus byggRespons(TpsNavEndringsMelding serviceRoutineRequest, ResponseStatus responseStatus) {
         String ident = ((TpsServiceRoutineEndringRequest) serviceRoutineRequest.getMelding()).getOffentligIdent();
         return ServiceRoutineResponseStatus.builder()
@@ -71,5 +88,5 @@ public class SendNavEndringsmeldinger {
                 .environment(serviceRoutineRequest.getMiljo())
                 .status(responseStatus).build();
     }
-    
+
 }
