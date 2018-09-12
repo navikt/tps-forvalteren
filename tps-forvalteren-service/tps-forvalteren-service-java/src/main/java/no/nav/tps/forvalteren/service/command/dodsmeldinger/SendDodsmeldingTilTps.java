faa9b6e4303cbbe1ec43fc83dc4648380cd543d9
Kristen Hærum <kristen.haerum@nav.no>
2018-09-12 09:17:34 +0200

    REG-963 SendDoedsmeldingTilTps refaktorering II

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/dodsmeldinger/SendDodsmeldingTilTps.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/dodsmeldinger/SendDodsmeldingTilTps.java
@@ -1,14 +1,11 @@
 package no.nav.tps.forvalteren.service.command.dodsmeldinger;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.google.common.collect.Sets;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import no.nav.tps.forvalteren.domain.jpa.DeathRow;
 import no.nav.tps.forvalteren.domain.jpa.Gateadresse;
 import no.nav.tps.forvalteren.domain.jpa.Person;
@@ -16,7 +13,6 @@ import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.definition.TpsSk
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.definition.resolvers.skdmeldinger.SkdMeldingResolver;
 import no.nav.tps.forvalteren.repository.jpa.DeathRowRepository;
 import no.nav.tps.forvalteren.service.command.testdata.skd.SendSkdMeldingTilGitteMiljoer;
-import no.nav.tps.forvalteren.service.command.testdata.skd.SkdMeldingTrans1;
 import no.nav.tps.forvalteren.service.command.testdata.skd.SkdMessageCreatorTrans1;
 
 @Service
@@ -29,28 +25,27 @@ public class SendDodsmeldingTilTps {
     private SkdMessageCreatorTrans1 skdCreator;
 
     @Autowired
-    private SendSkdMeldingTilGitteMiljoer sendSkdMeldingTilGitteMiljoer;
+    private SendSkdMeldingTilGitteMiljoer sendSkdMeldingTilMiljoe;
 
     @Autowired
-    private SkdMeldingResolver meldingResolver;
+    private SkdMeldingResolver innvandring;
 
     @Autowired
     private UpdateDeathRow updateDeathRow;
 
     public void execute() {
 
+        TpsSkdRequestMeldingDefinition skdRequestMeldingDefinition = innvandring.resolve();
         List<DeathRow> deathRowTasks = deathRowRepository.findAllByStatus("Ikke sendt");
 
-        List<Container> containere = new ArrayList<>();
-
-        for (DeathRow melding : deathRowTasks) {
+        for (DeathRow deathRow : deathRowTasks) {
 
             Person person = new Person();
-            person.setId(melding.getId());
-            person.setIdent(melding.getIdent());
+            person.setId(deathRow.getId());
+            person.setIdent(deathRow.getIdent());
             person.setRegdato(LocalDateTime.now());
 
-            if ("U".equals(melding.getHandling()) || "D".equals(melding.getHandling())) {
+            if ("U".equals(deathRow.getHandling()) || "D".equals(deathRow.getHandling())) {
                     /* Setter en dummy adresse*/
                 Gateadresse adr = new Gateadresse();
                 adr.setAdresse("SANNERGATA");
@@ -63,39 +58,27 @@ public class SendDodsmeldingTilTps {
                 adr.setKommunenr("1111");
                 person.setBoadresse(adr);
 
-                containere.add(new Container(createDoedsmeldingerAnnullering(person), melding.getMiljoe()));
+                sendSkdMeldingTilMiljoe.execute(createDoedsmeldingerAnnullering(person), skdRequestMeldingDefinition, Sets.newHashSet(deathRow.getMiljoe()));
             }
-            if ("U".equals(melding.getHandling()) || "C".equals(melding.getHandling())) {
-                person.setDoedsdato(melding.getDoedsdato());
-                person.setRegdato(melding.getDoedsdato());
-                containere.add(new Container(createDoedsmeldinger(person), melding.getMiljoe()));
+            if ("U".equals(deathRow.getHandling()) || "C".equals(deathRow.getHandling())) {
+                person.setDoedsdato(deathRow.getDoedsdato());
+
+                sendSkdMeldingTilMiljoe.execute(createDoedsmeldinger(person), skdRequestMeldingDefinition, Sets.newHashSet(deathRow.getMiljoe()));
             }
-            melding.setStatus("Sendt");
-            updateDeathRow.execute(melding);
-        }
 
-        TpsSkdRequestMeldingDefinition skdRequestMeldingDefinition = meldingResolver.resolve();
-
-        for (Container container : containere) {
-            sendSkdMeldingTilGitteMiljoer.execute(container.getSkdMeldingTrans1().toString(), skdRequestMeldingDefinition, Sets.newHashSet(container.getEnvironment()));
+            deathRow.setStatus("Sendt");
+            updateDeathRow.execute(deathRow);
         }
     }
 
-    private SkdMeldingTrans1 createDoedsmeldinger(Person person) {
+    private String createDoedsmeldinger(Person person) {
 
-        return skdCreator.execute("Doedsmelding", person, true);
+        return skdCreator.execute("Doedsmelding", person, true).toString();
     }
 
-    private SkdMeldingTrans1 createDoedsmeldingerAnnullering(Person person) {
+    private String createDoedsmeldingerAnnullering(Person person) {
 
-        return skdCreator.execute("DoedsmeldingAnnullering", person, true);
-    }
-
-    @Getter
-    @AllArgsConstructor
-    private class Container {
-        private SkdMeldingTrans1 skdMeldingTrans1;
-        private String environment;
+        return skdCreator.execute("DoedsmeldingAnnullering", person, true).toString();
     }
 }
 
