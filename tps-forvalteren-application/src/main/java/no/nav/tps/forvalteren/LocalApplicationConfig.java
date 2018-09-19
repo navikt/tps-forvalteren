8985405811d0f27c20df1e1e690aeb6d8ab47095
Kristen Hærum <kristen.haerum@nav.no>
2018-09-19 08:28:51 +0200

    REG-1007 Lagt til Flyway-script for seltting av DB artifacts. Rettet feil.

--- tps-forvalteren-application/src/main/java/no/nav/tps/forvalteren/LocalApplicationConfig.java
+++ tps-forvalteren-application/src/main/java/no/nav/tps/forvalteren/LocalApplicationConfig.java
@@ -1,14 +1,13 @@
 package no.nav.tps.forvalteren;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 import no.nav.tps.forvalteren.common.java.config.CommonConfig;
 import no.nav.tps.forvalteren.common.java.mapping.MapperConfig;
 import no.nav.tps.forvalteren.provider.rs.config.RestProviderConfig;
 import no.nav.tps.forvalteren.repository.jpa.config.RepositoryConfig;
-import no.nav.tps.forvalteren.skdavspilleren.repository.config.AvspillerRepositoryConfig;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableAutoConfiguration
@@ -16,8 +15,7 @@ import org.springframework.context.annotation.Import;
         CommonConfig.class,
         RepositoryConfig.class,
         MapperConfig.class,
-        RestProviderConfig.class,
-        AvspillerRepositoryConfig.class
+        RestProviderConfig.class
 })
 public class LocalApplicationConfig {
 
