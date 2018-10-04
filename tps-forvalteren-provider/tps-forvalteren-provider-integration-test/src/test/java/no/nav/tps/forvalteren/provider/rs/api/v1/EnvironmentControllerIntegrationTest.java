8af7e1544ed1c05734ae870db9004b80396ba529
Peter Flogstad <peter.sergio.flogstad@nav.no>
2018-10-04 12:27:07 +0200

    Slappet opp begrensninger for hvilke miljo man kan sende til i test

--- tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/EnvironmentControllerIntegrationTest.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/EnvironmentControllerIntegrationTest.java
@@ -1,5 +1,15 @@
 package no.nav.tps.forvalteren.provider.rs.api.v1;
 
+import no.nav.tps.forvalteren.provider.rs.AbstractRsProviderIntegrationTest;
+import no.nav.tps.forvalteren.provider.rs.api.v1.config.TestUserDetails;
+import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.EnvironmentController;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.util.ReflectionTestUtils;
+
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
@@ -7,17 +17,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.util.ReflectionUtils;
-
-import no.nav.tps.forvalteren.provider.rs.AbstractRsProviderIntegrationTest;
-import no.nav.tps.forvalteren.provider.rs.api.v1.config.TestUserDetails;
-import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.EnvironmentController;
 
 public class EnvironmentControllerIntegrationTest extends AbstractRsProviderIntegrationTest {
 
@@ -37,7 +36,7 @@ public class EnvironmentControllerIntegrationTest extends AbstractRsProviderInte
         mvc.perform(get("/api/v1/environments"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.environments", hasSize(16)))
+                .andExpect(jsonPath("$.environments", hasSize(29)))
                 .andExpect(jsonPath("$.environments", containsInAnyOrder(
                         "u5","u6", "t0","t1", "t2","t3","t4","t5", "t6", "t7","t8","t9","t10","t11","t12", "t13"
                 )))
