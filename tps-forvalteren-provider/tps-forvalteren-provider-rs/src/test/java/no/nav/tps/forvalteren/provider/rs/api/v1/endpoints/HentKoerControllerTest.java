06a6fe91eecd949cbe1e810bdf04d4f01be01f4f
Kristen Hærum <kristen.haerum@nav.no>
2018-10-11 09:18:52 +0200

    REG-313 Oppdatert tester

--- tps-forvalteren-provider/tps-forvalteren-provider-rs/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/HentKoerControllerTest.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-rs/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/endpoints/HentKoerControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.Collections;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -15,6 +16,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import no.nav.tps.forvalteren.consumer.rs.environments.FasitApiConsumer;
 import no.nav.tps.forvalteren.consumer.rs.environments.dao.FasitApplication;
 import no.nav.tps.forvalteren.consumer.rs.environments.mapper.MqInfoMapper;
+import no.nav.tps.forvalteren.service.command.exceptions.NotFoundException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HentKoerControllerTest {
@@ -35,6 +37,17 @@ public class HentKoerControllerTest {
 
     @Test
     public void getApplicationsOk() {
+        when(fasitApiConsumer.getApplications(APP)).thenReturn(Arrays.asList(fasitApplication));
+
+        hentKoerController.getApplications(APP);
+
+        verify(fasitApiConsumer).getApplications(APP);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getApplicationsEmptyList() {
+        when(fasitApiConsumer.getApplications(APP)).thenReturn(Collections.emptyList());
+
         hentKoerController.getApplications(APP);
 
         verify(fasitApiConsumer).getApplications(APP);
@@ -49,5 +62,14 @@ public class HentKoerControllerTest {
         verify(fasitApiConsumer).getApplicationInstances(APP, true);
         verify(fasitApiConsumer).getUsedResourcesFromAppByTypes(any(FasitApplication.class), any());
         verify(mqInfoMapper).execute(anyList(), anyList(), anyList());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getQueuesEmptyList() {
+        when(fasitApiConsumer.getApplicationInstances(APP, true)).thenReturn(Collections.emptyList());
+
+        hentKoerController.getQueues(APP);
+
+        verify(fasitApiConsumer).getApplicationInstances(APP, true);
     }
 }
