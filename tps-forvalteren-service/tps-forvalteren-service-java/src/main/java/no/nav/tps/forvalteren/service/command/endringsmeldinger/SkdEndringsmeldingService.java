3076ee0a42e4b866a4dbfd58ee7d7024ac5ac171
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-10-16 08:46:55 +0200

    REG-1708:Fjernet ubrukt kode

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/endringsmeldinger/SkdEndringsmeldingService.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/endringsmeldinger/SkdEndringsmeldingService.java
@@ -1,6 +1,5 @@
 package no.nav.tps.forvalteren.service.command.endringsmeldinger;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import no.nav.tps.forvalteren.domain.jpa.SkdEndringsmelding;
 import no.nav.tps.forvalteren.domain.jpa.SkdEndringsmeldingGruppe;
-import no.nav.tps.forvalteren.domain.rs.skd.RsMeldingstype;
 import no.nav.tps.forvalteren.repository.jpa.SkdEndringsmeldingGruppeRepository;
 import no.nav.tps.forvalteren.repository.jpa.SkdEndringsmeldingRepository;
 
@@ -21,19 +19,6 @@ public class SkdEndringsmeldingService {
     
     @Autowired
     private SkdEndringsmeldingGruppeRepository gruppeRepository;
-    
-    @Autowired
-    private ConvertJsonToRsMeldingstype convertJsonToRsMeldingstype;
-    
-    public List<RsMeldingstype> findAllSkdmeldingerByGruppeIdOrderByIdAsc(Long gruppeId) {
-        SkdEndringsmeldingGruppe gruppe = gruppeRepository.findById(gruppeId);
-        List<SkdEndringsmelding> skdEndringsmeldingList = skdEndringsmeldingRepository.findAllByGruppeOrderByIdAsc(gruppe);
-        List<RsMeldingstype> rsSkdmeldingene = new ArrayList<>();
-        for (SkdEndringsmelding mld : skdEndringsmeldingList) {
-            rsSkdmeldingene.add(convertJsonToRsMeldingstype.execute(mld));
-        }
-        return rsSkdmeldingene;
-    }
     
     public Set<String> filtrerIdenterPaaAarsakskodeOgTransaksjonstype(Long gruppeId, List<String> aarsakskoder, String transaksjonstype) {
         SkdEndringsmeldingGruppe gruppe = gruppeRepository.findById(gruppeId);
