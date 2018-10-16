78398b3460bdb8e1f217ae961f7bc7890de2f578
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-10-16 09:42:12 +0200

    REG-1708: Fjernet identpoolConsumer. Blir ikke innført i TPSF i denne branchen. Venter på at identpool er klar til bruk

--- tps-forvalteren-consumer/tps-forvalteren-consumer-rs/src/main/java/no/nav/tps/forvalteren/consumer/rs/config/RestConsumerConfig.java
+++ tps-forvalteren-consumer/tps-forvalteren-consumer-rs/src/main/java/no/nav/tps/forvalteren/consumer/rs/config/RestConsumerConfig.java
@@ -1,18 +1,16 @@
 package no.nav.tps.forvalteren.consumer.rs.config;
 
-import no.nav.tps.forvalteren.consumer.rs.environments.config.FetchEnvironmentsConsumerConfig;
-import no.nav.tps.forvalteren.consumer.rs.fasit.config.FasitConfig;
-import no.nav.tps.forvalteren.consumer.rs.identpool.IdentPoolClient;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import no.nav.tps.forvalteren.consumer.rs.environments.config.FetchEnvironmentsConsumerConfig;
+import no.nav.tps.forvalteren.consumer.rs.fasit.config.FasitConfig;
 
 
 @Configuration
 @Import({
         FetchEnvironmentsConsumerConfig.class,
-        FasitConfig.class,
-        IdentPoolClient.class
+        FasitConfig.class
 })
 public class RestConsumerConfig {
 }
