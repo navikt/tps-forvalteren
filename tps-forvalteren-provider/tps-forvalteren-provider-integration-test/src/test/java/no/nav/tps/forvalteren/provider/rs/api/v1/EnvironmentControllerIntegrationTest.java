79c52f06100a1ff50fea8927cb8a4a234f25e2ae
Kristen Hærum <kristen.haerum@nav.no>
2018-10-22 12:59:27 +0200

    REG-1780 Oppdatert dødsmeldinger til å håndtere flere miljøer i samme melding

--- tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/EnvironmentControllerIntegrationTest.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-integration-test/src/test/java/no/nav/tps/forvalteren/provider/rs/api/v1/EnvironmentControllerIntegrationTest.java
@@ -1,15 +1,5 @@
 package no.nav.tps.forvalteren.provider.rs.api.v1;
 
-import no.nav.tps.forvalteren.provider.rs.AbstractRsProviderIntegrationTest;
-import no.nav.tps.forvalteren.provider.rs.api.v1.config.TestUserDetails;
-import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.EnvironmentController;
-
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.util.ReflectionTestUtils;
-
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
@@ -17,6 +7,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import no.nav.tps.forvalteren.provider.rs.AbstractRsProviderIntegrationTest;
+import no.nav.tps.forvalteren.provider.rs.api.v1.config.TestUserDetails;
+import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.EnvironmentController;
 
 public class EnvironmentControllerIntegrationTest extends AbstractRsProviderIntegrationTest {
 
@@ -32,16 +32,15 @@ public class EnvironmentControllerIntegrationTest extends AbstractRsProviderInte
     @WithUserDetails(TestUserDetails.USERNAME)
     public void getsEnvironments() throws Exception {
 
-        ReflectionTestUtils.setField(environmentController,"currentEnvironmentIsProd", false);
+        ReflectionTestUtils.setField(environmentController, "currentEnvironmentIsProd", false);
         mvc.perform(get("/api/v1/environments"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.environments", hasSize(29)))
+                .andExpect(jsonPath("$.environments", hasSize(28)))
                 .andExpect(jsonPath("$.environments", containsInAnyOrder(
-                        "u5","u6", "t0","t1", "t2","t3","t4","t5", "t6", "t7","t8","t9","t10","t11","t12", "t13",
-                            "q11","q10","q9","q8","q7", "q6", "q5", "q4", "q3", "q2", "q1", "q0","qx"
+                        "u5", "u6", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t10", "t11", "t12", "t13",
+                        "q11", "q10", "q9", "q8", "q7", "q6", "q5", "q4", "q3", "q2", "q1", "q0", "qx"
                 )))
                 .andExpect(jsonPath("$.productionMode", is(false)));
     }
-
 }
