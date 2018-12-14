eaf994ebc3096733721970e00f4c4eddcc638192
Kristen Hærum <kristen.haerum@nav.no>
2018-12-14 12:37:45 +0100

    REG-2581 Utvidet antall mulige fødselsnummere ved å begrense til 19- og 20-århundre kun, lagt på validering

--- tps-forvalteren-common/tps-forvalteren-common-java/src/main/java/no/nav/tps/forvalteren/common/java/message/MessageConstants.java
+++ tps-forvalteren-common/tps-forvalteren-common-java/src/main/java/no/nav/tps/forvalteren/common/java/message/MessageConstants.java
@@ -1,22 +1,15 @@
 package no.nav.tps.forvalteren.common.java.message;
 
+public final class MessageConstants {
 
-public class MessageConstants {
+    private MessageConstants() {
+    }
+
     public static final String UNKNOWN_MESSAGE_KEY = "messages.unknown.message.key";
-
-    public static final String SELFTEST_UNKNOWN_ERROR_KEY = "selftest.result.unknown.error";
-    public static final String SELFTEST_EXCEPTION_MESSAGE_KEY = "selftest.result.exception.message";
 
     public static final String GRUPPE_NOT_FOUND_KEY = "service.exception.message.gruppe.not.found";
 
-    public static final String TPS_CALL_FORBIDDEN = "service.exception.message.tps.call.forbidden";
-    public static final String MQ_SENDING_FAILED = "service.exception.message.mq.sending.failed";
-
-    public static final String MAPPING_FAILED = "service.exception.message.mapping.failed";
-
     public static final String KODEVERK_NOT_FOUND_KEY = "ws.kodeverk.v2.kodeverk.not.found";
-    public static final String UPDATING_KODEVERK_KEY = "service.kodeverk.updating";
-    public static final String GETTING_KODEVERK_KOMMUNER_FAILED = "service.kodeverk.fetching";
 
     public static final String UNKNOWN_SKD_PARAMETER_STRATEGY = "service.exception.parameter.mapping.failed";
 
@@ -27,4 +20,8 @@ public class MessageConstants {
     public static final String SKD_ENDRINGSMELDING_JSON_TO_OBJECT = "service.exception.message.skd.json.to.object";
     public static final String SKD_ENDRINGSMELDING_ILLEGAL_LENGTH = "service.exception.message.skd.illegal.length";
 
+    public static final String BESTILLING_VALIDERING_FOEDT_ETTER = "bestilling.input.validation.dato.foedt.etter";
+    public static final String BESTILLING_VALIDERING_FOEDT_FOER = "bestilling.input.validation.dato.foedt.foer";
+    public static final String BESTILLING_VALIDERING_UGYLDIG_INTERVALL = "bestilling.input.validation.ugyldig.intervall";
+    public static final String BESTILLING_VALIDERING_UGYLDIG_KJOENN = "bestilling.input.validation.ugyldig.kjoenn";
 }
---
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/restreq/ValidateOpprettRequest.java
@@ -0,0 +1,71 @@
+package no.nav.tps.forvalteren.service.command.testdata.restreq;
+
+import static java.util.Objects.nonNull;
+import static no.nav.tps.forvalteren.common.java.message.MessageConstants.BESTILLING_VALIDERING_FOEDT_ETTER;
+import static no.nav.tps.forvalteren.common.java.message.MessageConstants.BESTILLING_VALIDERING_FOEDT_FOER;
+import static no.nav.tps.forvalteren.common.java.message.MessageConstants.BESTILLING_VALIDERING_UGYLDIG_INTERVALL;
+import static no.nav.tps.forvalteren.common.java.message.MessageConstants.BESTILLING_VALIDERING_UGYLDIG_KJOENN;
+
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import no.nav.tps.forvalteren.common.java.message.MessageProvider;
+import no.nav.tps.forvalteren.domain.rs.dolly.RsPersonBestillingKriteriumRequest;
+import no.nav.tps.forvalteren.service.command.exceptions.TpsfFunctionalException;
+
+@Component
+public class ValidateOpprettRequest {
+
+    private static final LocalDateTime LOWER_BOUND = LocalDateTime.of(1900, 01, 01, 00, 00, 00);
+    private static final LocalDateTime UPPER_BOUND = LocalDateTime.of(2039, 12, 31, 23, 59, 59);
+
+    @Autowired
+    private MessageProvider messageProvider;
+
+    public void validate(RsPersonBestillingKriteriumRequest request) {
+
+        validateDatoFoedtEtter(request.getFoedtEtter());
+        validateDatoFoedtFoer(request.getFoedtFoer());
+        validateDatoIntervall(request.getFoedtEtter(), request.getFoedtFoer());
+        validateKjoenn(request.getKjonn());
+
+        if (nonNull(request.getRelasjoner().getPartner())) {
+            validateDatoFoedtEtter(request.getRelasjoner().getPartner().getFoedtEtter());
+            validateDatoFoedtFoer(request.getRelasjoner().getPartner().getFoedtFoer());
+            validateDatoIntervall(request.getRelasjoner().getPartner().getFoedtEtter(), request.getRelasjoner().getPartner().getFoedtFoer());
+            validateKjoenn(request.getRelasjoner().getPartner().getKjonn());
+        }
+
+        if (!request.getRelasjoner().getBarn().isEmpty()) {
+            request.getRelasjoner().getBarn().forEach(barn -> validateDatoFoedtEtter(barn.getFoedtEtter()));
+            request.getRelasjoner().getBarn().forEach(barn -> validateDatoFoedtFoer(barn.getFoedtFoer()));
+            request.getRelasjoner().getBarn().forEach(barn -> validateDatoIntervall(barn.getFoedtEtter(), barn.getFoedtFoer()));
+            request.getRelasjoner().getBarn().forEach(barn -> validateKjoenn(barn.getKjonn()));
+        }
+    }
+
+    private void validateDatoFoedtEtter(LocalDateTime datoFoedtEtter) {
+        if (nonNull(datoFoedtEtter) && datoFoedtEtter.isAfter(UPPER_BOUND)) {
+            throw new TpsfFunctionalException(messageProvider.get(BESTILLING_VALIDERING_FOEDT_ETTER));
+        }
+    }
+
+    private void validateDatoFoedtFoer(LocalDateTime datoFoedtFoer) {
+        if (nonNull(datoFoedtFoer) && datoFoedtFoer.isBefore(LOWER_BOUND)) {
+            throw new TpsfFunctionalException(messageProvider.get(BESTILLING_VALIDERING_FOEDT_FOER));
+        }
+    }
+
+    private void validateDatoIntervall(LocalDateTime datoFoedtEtter, LocalDateTime datoFoedtFoer) {
+        if (nonNull(datoFoedtEtter) && nonNull(datoFoedtFoer) && datoFoedtFoer.isBefore(datoFoedtEtter)) {
+            throw new TpsfFunctionalException(messageProvider.get(BESTILLING_VALIDERING_UGYLDIG_INTERVALL));
+        }
+    }
+
+    private void validateKjoenn(String kjoenn) {
+        if (nonNull(kjoenn) && !"M".equals(kjoenn) && !"K".equals(kjoenn) && !"U".equals(kjoenn)) {
+            throw new TpsfFunctionalException(messageProvider.get(BESTILLING_VALIDERING_UGYLDIG_KJOENN));
+        }
+    }
+}
