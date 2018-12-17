b14c4d9bcdb513514e1a7ea269cab1c3752521dd
Kristen Hærum <kristen.haerum@nav.no>
2018-12-17 12:05:45 +0100

    REG-2660 Lagt på constructor for JSON

--- tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsSimplePersonRequest.java
+++ tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsSimplePersonRequest.java
@@ -2,13 +2,17 @@ package no.nav.tps.forvalteren.domain.rs;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RsSimplePersonRequest {
 
     private String identtype;
