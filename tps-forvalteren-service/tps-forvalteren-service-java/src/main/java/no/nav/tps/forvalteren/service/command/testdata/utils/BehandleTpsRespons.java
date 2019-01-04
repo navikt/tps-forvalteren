2a52e9acaf23458c390decb9a48d9450772e9a2a
Kristen Hærum <kristen.haerum@nav.no>
2019-01-04 10:45:08 +0100

    REG-2293 Lagt på egenansatt på partner og barn; forbedret Json på tomme statusfelter; forbedret testdekning

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/utils/BehandleTpsRespons.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/utils/BehandleTpsRespons.java
@@ -1,8 +1,9 @@
 package no.nav.tps.forvalteren.service.command.testdata.utils;
 
 import static no.nav.tps.forvalteren.service.command.tps.servicerutiner.utils.RsTpsResponseMappingUtils.STATUS_KEY;
+import static no.nav.tps.forvalteren.service.command.tps.skdmelding.skdparam.utils.NullcheckUtil.nullcheckSetDefaultValue;
 
-import java.util.LinkedHashMap;
+import java.util.Map;
 
 import no.nav.tps.forvalteren.domain.service.tps.ResponseStatus;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.response.TpsServiceRoutineResponse;
@@ -13,7 +14,9 @@ public final class BehandleTpsRespons {
     }
 
     public static ResponseStatus ekstraherStatusFraServicerutineRespons(TpsServiceRoutineResponse svar) {
-        LinkedHashMap map = (LinkedHashMap) svar.getResponse();
-         return (ResponseStatus) map.get(STATUS_KEY);
+        ResponseStatus responseStatus = (ResponseStatus) ((Map) svar.getResponse()).get(STATUS_KEY);
+        responseStatus.setMelding(nullcheckSetDefaultValue(responseStatus.getMelding(), null));
+        responseStatus.setUtfyllendeMelding(nullcheckSetDefaultValue(responseStatus.getUtfyllendeMelding(), null));
+        return responseStatus;
     }
 }
