abc6c95f1d5ae1d950bf8534dfcbe3bfd31fe4a0
Kristen Hærum <kristen.haerum@nav.no>
2019-11-25 10:23:52 +0100

    REG-6271 Oppdatert funksjon på henting av identer mot miljø

--- tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/TpsStatusPaaIdent.java
+++ tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/TpsStatusPaaIdent.java
@@ -4,23 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TpsStatusPaaIdent {
     
     private String ident;
     private List<String> env;
-    
-    public void addEnv(String environment) {
-        getEnv().add(environment);
-    }
-    
+
     public List<String> getEnv() {
         if (env == null) {
             env = new ArrayList<>();
--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/utils/ExtractDataFromTpsServiceRoutineResponse.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/utils/ExtractDataFromTpsServiceRoutineResponse.java
@@ -1,29 +1,52 @@
 package no.nav.tps.forvalteren.service.command.testdata.utils;
 
+import static java.util.Objects.nonNull;
+
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import lombok.NoArgsConstructor;
+import no.nav.tps.forvalteren.domain.service.tps.ResponseStatus;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.response.TpsServiceRoutineResponse;
 
+@NoArgsConstructor
 public final class ExtractDataFromTpsServiceRoutineResponse {
-
-    private ExtractDataFromTpsServiceRoutineResponse() {
-
-    }
 
     public static Set<String> trekkUtIdenterMedStatusIkkeFunnetFraResponse(TpsServiceRoutineResponse tpsResponse) {
 
         Map responseMap = (Map) tpsResponse.getResponse();
         int antallIdenter = (int) responseMap.get("antallTotalt");
 
-        Set<String> identer = new HashSet(antallIdenter);
+        Set<String> identer = new HashSet();
 
-        for (int i = 1; i < antallIdenter + 1; i++) {
-            Map data = (Map) responseMap.get("data" + i);
-            identer.add(String.valueOf(data.get("fnr")));
+        ResponseStatus status = (ResponseStatus) getArtifact(responseMap, "status");
+        if (!"08".equals(status.getKode())) {
+            for (int i = 1; i < antallIdenter + 1; i++) {
+                Map data = (Map) getArtifact(responseMap, "data" + i);
+
+                Map aFnr = (Map) getArtifact(data, "AFnr");
+                Map svarStatus;
+                if (nonNull(aFnr)) {
+                    Map eFnr = (Map) getArtifact(aFnr, "EFnr");
+                    svarStatus = (Map) getArtifact(data, "svarStatus");
+                    String returStatus = (String) getArtifact(svarStatus, "returStatus");
+                    if (!"08".equals(returStatus)) {
+                        identer.add(String.valueOf(eFnr.get("fnr")));
+                    }
+                } else {
+                    svarStatus = (Map) getArtifact(data, "svarStatus");
+                    String returStatus = (String) getArtifact(svarStatus, "returStatus");
+                    if (!"08".equals(returStatus)) {
+                        identer.add(String.valueOf(data.get("fnr")));
+                    }
+                }
+            }
         }
-        
         return identer;
+    }
+
+    private static Object getArtifact(Map map, String key) {
+        return nonNull(map) ? map.get(key) : null;
     }
 }
