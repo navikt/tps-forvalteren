f3ad5632d9cb69cf7587560acb0e62cc3241a6a7
Kristen Hærum <kristen.haerum@nav.no>
2020-02-09 11:33:09 +0100

    REG-5947 Oppdatert konfig III

--- tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/config/RestProviderConfig.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-rs/src/main/java/no/nav/tps/forvalteren/provider/rs/config/RestProviderConfig.java
@@ -1,25 +1,31 @@
 package no.nav.tps.forvalteren.provider.rs.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import no.nav.tps.forvalteren.provider.rs.api.v1.documentation.SwaggerConfig;
+import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.UserController;
+import no.nav.tps.forvalteren.provider.rs.api.v1.endpoints.advices.HttpExceptionAdvice;
+import no.nav.tps.forvalteren.provider.rs.naisendpoints.NaisEndpointController;
+import no.nav.tps.forvalteren.provider.rs.security.config.RestSecurityConfig;
 import no.nav.tps.forvalteren.provider.rs.security.config.WebSecurityConfig;
+import no.nav.tps.forvalteren.service.config.ServiceConfig;
 
 @Configuration
 @Import({
-//        ServiceConfig.class,
-//        SwaggerConfig.class,
+        ServiceConfig.class,
+        SwaggerConfig.class,
         WebSecurityConfig.class,
-//        RestSecurityConfig.class
+        RestSecurityConfig.class
 })
-//@ComponentScan(basePackageClasses = {
-//        UserController.class,
-//        NaisEndpointController.class,
-//        HttpExceptionAdvice.class,
-//        RestAuthorizationService.class
-//})
+@ComponentScan(basePackageClasses = {
+        UserController.class,
+        NaisEndpointController.class,
+        HttpExceptionAdvice.class
+})
 public class RestProviderConfig extends WebMvcConfigurerAdapter {
     
     @Override
