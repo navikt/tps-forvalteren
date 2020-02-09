4205d6afedeae36330a91a8529a74b11c744879b
Kristen Hærum <kristen.haerum@nav.no>
2020-02-09 12:05:05 +0100

    REG-5947 Oppdatert konfig V

--- tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/java/no/nav/tps/forvalteren/provider/web/config/WebProviderConfig.java
+++ tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/java/no/nav/tps/forvalteren/provider/web/config/WebProviderConfig.java
@@ -2,12 +2,16 @@ package no.nav.tps.forvalteren.provider.web.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import no.nav.tps.forvalteren.common.java.config.CommonConfig;
+import no.nav.tps.forvalteren.provider.rs.config.RestProviderConfig;
 
 @Configuration
 @EnableConfigurationProperties
-//@Import({ RestProviderConfig.class,
-//        CommonConfig.class,
-//})
+@Import({ RestProviderConfig.class,
+        CommonConfig.class,
+})
 public class WebProviderConfig {
 
 }
