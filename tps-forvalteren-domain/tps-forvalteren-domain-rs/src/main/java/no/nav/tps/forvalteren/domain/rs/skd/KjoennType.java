345c048ec5a2595d42683410aebbaea2239d1336
Kristen Hærum <kristen.haerum@nav.no>
2020-05-06 10:04:06 +0200

    REG-8045 Endret til at 5% av par der kjønn ikke er eksplisitt satt blir homofile

--- tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/skd/KjoennType.java
+++ tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/skd/KjoennType.java
@@ -2,5 +2,16 @@ package no.nav.tps.forvalteren.domain.rs.skd;
 
 public enum KjoennType {
 
-    K, M, U
+    K, M, U;
+
+    public static KjoennType getMotsattKjoenn (KjoennType kjoenn) {
+
+        if (M == kjoenn) {
+            return K;
+        } else if (K == kjoenn) {
+            return M;
+        } else {
+            return kjoenn;
+        }
+    }
 }
--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/restreq/EnforceForeldreKjoennService.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/restreq/EnforceForeldreKjoennService.java
@@ -20,13 +20,23 @@ public class EnforceForeldreKjoennService {
                 request.setKjonn(random.nextBoolean() ? KjoennType.K : KjoennType.M);
 
             } else if (!request.getRelasjoner().getPartnere().isEmpty()) {
-                request.setKjonn(request.getRelasjoner().getPartnere().get(0).isMann() ? KjoennType.K : KjoennType.M);
+                request.setKjonn(request.getRelasjoner().getPartnere().get(0).isMann() ?
+                        getWeightedKjonn(KjoennType.K) : getWeightedKjonn(KjoennType.M));
             }
         }
         request.getRelasjoner().getPartnere().forEach(partner -> {
             if (!partner.hasSpesificKjoenn()) {
-                partner.setKjonn(request.isMann() ? KjoennType.K : KjoennType.M);
+                partner.setKjonn(request.isMann() ? getWeightedKjonn(KjoennType.K) : getWeightedKjonn(KjoennType.M));
             }
         });
+    }
+
+    private KjoennType getWeightedKjonn(KjoennType kjoenn) {
+
+        if (random.nextFloat() <= .95) {
+            return kjoenn;
+        } else {
+            return KjoennType.getMotsattKjoenn(kjoenn);
+        }
     }
 }
