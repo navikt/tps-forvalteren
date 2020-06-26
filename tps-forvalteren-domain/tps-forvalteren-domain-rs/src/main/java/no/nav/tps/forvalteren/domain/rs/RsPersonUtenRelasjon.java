7150c479c9f7b316bf27fbc2356b08266c65c6d4
Kristen Hærum <kristen.haerum@nav.no>
2020-06-26 15:47:31 +0200

    REG-8613 Fikset problem med null for tomme lister for relasjoner også

--- tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsPersonUtenRelasjon.java
+++ tps-forvalteren-domain/tps-forvalteren-domain-rs/src/main/java/no/nav/tps/forvalteren/domain/rs/RsPersonUtenRelasjon.java
@@ -19,7 +19,7 @@ import no.nav.tps.forvalteren.domain.jpa.Statsborgerskap;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RsPersonUtenRelasjon {
 
     private Long personId;
