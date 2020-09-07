7d41521a942feab8afaf3f91e21243595b291247
Kristen Hærum <kristen.herum@nav.no>
2020-09-07 14:12:40 +0200

    Lagt til graceful fallback når enkeltmiljøer feiler å nås

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/StatusPaaIdenterITps.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/StatusPaaIdenterITps.java
@@ -11,15 +11,17 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.tps.forvalteren.domain.rs.RsTpsStatusPaaIdenterResponse;
 import no.nav.tps.forvalteren.domain.rs.TpsStatusPaaIdent;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.TpsRequestContext;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.requests.TpsServiceRoutineRequest;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.response.TpsServiceRoutineResponse;
 import no.nav.tps.forvalteren.service.command.FilterEnvironmentsOnDeployedEnvironment;
+import no.nav.tps.forvalteren.service.command.exceptions.HttpInternalServerErrorException;
 import no.nav.tps.forvalteren.service.command.tps.servicerutiner.TpsRequestSender;
 import no.nav.tps.forvalteren.service.command.tps.servicerutiner.utils.RsTpsRequestMappingUtils;
 import no.nav.tps.forvalteren.service.command.tpsconfig.GetEnvironments;
@@ -28,23 +30,16 @@ import no.nav.tps.forvalteren.service.user.UserContextHolder;
 /**
  * Denne tjenesten returnerer hvilke miljøer som de ulike fødselsnumrene eksisterer i.
  */
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class StatusPaaIdenterITps {
 
-    @Autowired
-    private GetEnvironments getEnvironments;
-
-    @Autowired
-    private FilterEnvironmentsOnDeployedEnvironment filterEnvironmentsOnDeployedEnvironment;
-
-    @Autowired
-    private UserContextHolder userContextHolder;
-
-    @Autowired
-    private TpsRequestSender tpsRequestSender;
-
-    @Autowired
-    private RsTpsRequestMappingUtils mappingUtils;
+    private final GetEnvironments getEnvironments;
+    private final FilterEnvironmentsOnDeployedEnvironment filterEnvironmentsOnDeployedEnvironment;
+    private final UserContextHolder userContextHolder;
+    private final TpsRequestSender tpsRequestSender;
+    private final RsTpsRequestMappingUtils mappingUtils;
 
     public RsTpsStatusPaaIdenterResponse hentStatusPaaIdenterIAlleMiljoer(List<String> identer) {
 
@@ -59,14 +54,18 @@ public class StatusPaaIdenterITps {
 
         environmentsToCheck.forEach(env -> {
 
-            TpsServiceRoutineResponse tpsResponse = tpsRequestSender.sendTpsRequest(tpsServiceRoutineRequest,
-                    new TpsRequestContext(userContextHolder.getUser(), env));
-            trekkUtIdenterMedStatusFunnetFraResponse(tpsResponse).forEach(ident -> {
-                        Set miljoer = identerPerMiljoe.get(ident);
-                        miljoer.add(env);
-                        identerPerMiljoe.put(ident, miljoer);
-                    }
-            );
+            try {
+                TpsServiceRoutineResponse tpsResponse = tpsRequestSender.sendTpsRequest(tpsServiceRoutineRequest,
+                        new TpsRequestContext(userContextHolder.getUser(), env));
+                trekkUtIdenterMedStatusFunnetFraResponse(tpsResponse).forEach(ident -> {
+                            Set miljoer = identerPerMiljoe.get(ident);
+                            miljoer.add(env);
+                            identerPerMiljoe.put(ident, miljoer);
+                        }
+                );
+            } catch (HttpInternalServerErrorException e) {
+                log.warn("Feilet å sjekke eksistensstatus for ident i miljoe {}", env);
+            }
         });
 
         return RsTpsStatusPaaIdenterResponse.builder()
