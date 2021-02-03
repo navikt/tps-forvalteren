2bac5d299184de58ae9fa3189a63822c18979bc7
Kristen Hærum <kristen.herum@nav.no>
2021-02-03 07:46:33 +0100

    Fikset problem hashcode på Fullmakt

--- tps-forvalteren-domain/tps-forvalteren-domain-jpa/src/main/java/no/nav/tps/forvalteren/domain/jpa/Fullmakt.java
+++ tps-forvalteren-domain/tps-forvalteren-domain-jpa/src/main/java/no/nav/tps/forvalteren/domain/jpa/Fullmakt.java
@@ -13,11 +13,13 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "T_FULLMAKT")
 @NoArgsConstructor
 @AllArgsConstructor
