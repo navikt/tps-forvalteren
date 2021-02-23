81c1dafbbbe17af9f9a7189ce183894b2dc31c21
Stian <stian.gustavsson@nav.no>
2021-02-23 11:14:47 +0100

    Returnerer 404 istedenfor 500 på /personstatus-og-adresse-person endepunkt dersom ident ikke finnes

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/tps/servicerutiner/TpsResponseToJsonHandler.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/tps/servicerutiner/TpsResponseToJsonHandler.java
@@ -3,20 +3,25 @@ package no.nav.tps.forvalteren.service.command.tps.servicerutiner;
 import no.nav.tps.forvalteren.domain.service.tps.ResponseStatus;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.response.TpsServiceRoutineResponse;
 import no.nav.tps.forvalteren.service.command.exceptions.ExceptionTpsServiceRutineMessageCreator;
+import no.nav.tps.forvalteren.service.command.exceptions.NotFoundException;
 import no.nav.tps.forvalteren.service.command.exceptions.TpsServiceRutineException;
+import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import org.springframework.stereotype.Service;
 
 @Service
 public class TpsResponseToJsonHandler {
 
-    public Map execute(TpsServiceRoutineResponse tpsResponse){
-        ResponseStatus status = (ResponseStatus) ((LinkedHashMap)tpsResponse.getResponse()).get("status");
+    public Map execute(TpsServiceRoutineResponse tpsResponse) {
+        ResponseStatus status = (ResponseStatus) ((LinkedHashMap) tpsResponse.getResponse()).get("status");
 
-        if("00".equals(status.getKode())){
+        if ("00".equals(status.getKode())) {
             return (Map) ((LinkedHashMap) tpsResponse.getResponse()).get("data1");
+        }
+
+        if ("08".equals(status.getKode()) && status.getUtfyllendeMelding().contains("finnes ikke")) {
+            throw new NotFoundException(status.getUtfyllendeMelding());
         }
 
         throw new TpsServiceRutineException(ExceptionTpsServiceRutineMessageCreator.execute(status));
