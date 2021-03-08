8f141d6119fd57c8fb91233b3bacbf1023a7e457
krharum <bc05aab8d40e1cec7718d2efaef17f0817190a61>
2021-03-08 14:23:10 +0100

    Fjernet overflødig kode

--- tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/rawxml-melding/xml-melding.controller.js
+++ tps-forvalteren-provider/tps-forvalteren-provider-web/src/main/web-src/src/app/components/rawxml-melding/xml-melding.controller.js
@@ -5,57 +5,10 @@ angular.module('tps-forvalteren.rawxml-melding', ['ngMaterial'])
             headerService.setHeader('Send XML-melding');
 
             $scope.melding = '';
-            $scope.displayQueues = [];
-            $scope.displayEnvironments = [];
             $scope.showResponse = false;
             $scope.responseMelding = "";
             $scope.timeout = 5;
-            $scope.applications = [];
             var appobjects = [];
-
-            $scope.findApps = function (inp) {
-
-                xmlmeldingService.hentApplikasjoner(inp).then(function (result) {
-
-                    $scope.applications = [];
-                    result.data.forEach(function (app) {
-                        if (!utilsService.arrayContains($scope.applications, app.name)) {
-                            $scope.applications.push(app.name);
-                        }
-                    });
-
-                }, function(error) {
-                    utilsService.showAlertError(error);
-                });
-            };
-
-            $scope.onChangeApp = function () {
-
-                xmlmeldingService.hentAppRessurser($scope.valgtApp).then(function (result) {
-
-                    prepAppResources(result.data);
-                });
-            };
-
-            function prepAppResources(result) {
-                appobjects = result;
-                var environments = [];
-                var queues = [];
-
-                appobjects.forEach(function (app) {
-                    if (!utilsService.arrayContains(environments, app.environment)) {
-                        environments.push(app.environment);
-                    }
-                    app.queues.forEach(function (que) {
-                        if (!utilsService.arrayContains(queues, que.queueName)) {
-                            queues.push(que.queueName);
-                        }
-                    });
-                });
-
-                $scope.displayQueues = queues;
-                $scope.displayEnvironments = sortEnvironmentsForDisplay(environments);
-            }
 
             $scope.sendTilTps = function () {
                 var objectToTps = {
