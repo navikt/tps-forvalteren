ae13044ab8e924fafe51f151c975805a2f8871a2
krharum <bc05aab8d40e1cec7718d2efaef17f0817190a61>
2021-03-15 19:18:41 +0100

    Forbedret ytelse på sletting ved å sende parallelt til alle miljøer

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/skd/SendSkdMeldingTilGitteMiljoer.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/skd/SendSkdMeldingTilGitteMiljoer.java
@@ -1,34 +1,27 @@
 package no.nav.tps.forvalteren.service.command.testdata.skd;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import no.nav.tps.forvalteren.domain.service.tps.servicerutiner.definition.TpsSkdRequestMeldingDefinition;
 import no.nav.tps.forvalteren.service.command.FilterEnvironmentsOnDeployedEnvironment;
 import no.nav.tps.forvalteren.service.command.testdata.skd.impl.SendEnSkdMelding;
 
 @Service
+@RequiredArgsConstructor
 public class SendSkdMeldingTilGitteMiljoer {
-    
-    @Autowired
-    private SendEnSkdMelding sendEnSkdMelding;
-    
-    @Autowired
-    private FilterEnvironmentsOnDeployedEnvironment filterEnvironmentsOnDeployedEnvironment;
-    
+
+    private final SendEnSkdMelding sendEnSkdMelding;
+    private final FilterEnvironmentsOnDeployedEnvironment filterEnvironmentsOnDeployedEnvironment;
+
     public Map<String, String> execute(String skdMelding, TpsSkdRequestMeldingDefinition skdRequestMeldingDefinition, Set<String> environments) {
 
         Set<String> envToCheck = filterEnvironmentsOnDeployedEnvironment.execute(environments);
-        Map<String, String> responseSkdMeldinger = new HashMap(envToCheck.size());
-
-        for (String env : envToCheck) {
-            String responsMelding = sendEnSkdMelding.sendSkdMelding(skdMelding, skdRequestMeldingDefinition, env);
-            responseSkdMeldinger.put(env, responsMelding);
-        }
-        return responseSkdMeldinger;
+        return envToCheck.parallelStream()
+                .collect(Collectors.toMap(env -> env, env -> sendEnSkdMelding.sendSkdMelding(skdMelding, skdRequestMeldingDefinition, env)));
     }
 
     public String execute(String skdMelding, TpsSkdRequestMeldingDefinition skdRequestMeldingDefinition, String environment) {
