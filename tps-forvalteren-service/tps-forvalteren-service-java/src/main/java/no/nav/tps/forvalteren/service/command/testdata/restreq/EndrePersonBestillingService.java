9629de0751e252e64721925b17893a8d3d006b27
Kristen Hærum <kristen.herum@nav.no>
2021-03-22 14:33:26 +0100

    Lagt til støtte for endring av identtype

--- tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/restreq/EndrePersonBestillingService.java
+++ tps-forvalteren-service/tps-forvalteren-service-java/src/main/java/no/nav/tps/forvalteren/service/command/testdata/restreq/EndrePersonBestillingService.java
@@ -1,23 +1,5 @@
 package no.nav.tps.forvalteren.service.command.testdata.restreq;
 
-import lombok.RequiredArgsConstructor;
-import ma.glasnost.orika.MapperFacade;
-import no.nav.tps.forvalteren.common.message.MessageProvider;
-import no.nav.tps.forvalteren.domain.jpa.Adresse;
-import no.nav.tps.forvalteren.domain.jpa.Person;
-import no.nav.tps.forvalteren.domain.jpa.Relasjon;
-import no.nav.tps.forvalteren.domain.jpa.Statsborgerskap;
-import no.nav.tps.forvalteren.domain.rs.dolly.RsOppdaterPersonResponse;
-import no.nav.tps.forvalteren.domain.rs.dolly.RsPersonBestillingKriteriumRequest;
-import no.nav.tps.forvalteren.repository.jpa.PersonRepository;
-import no.nav.tps.forvalteren.service.command.exceptions.TpsfFunctionalException;
-import no.nav.tps.forvalteren.service.command.testdata.opprett.RandomAdresseService;
-import no.nav.tps.forvalteren.service.command.testdata.utils.HentDatoFraIdentService;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static java.lang.String.format;
 import static java.time.LocalDateTime.now;
 import static java.util.Objects.isNull;
@@ -25,7 +7,31 @@ import static java.util.Objects.nonNull;
 import static no.nav.tps.forvalteren.domain.jpa.InnvandretUtvandret.InnUtvandret.INNVANDRET;
 import static no.nav.tps.forvalteren.domain.jpa.InnvandretUtvandret.InnUtvandret.UTVANDRET;
 import static no.nav.tps.forvalteren.domain.rs.skd.IdentType.FNR;
+import static no.nav.tps.forvalteren.service.command.testdata.restreq.OpprettPersonUtil.extractMainPerson;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import ma.glasnost.orika.MapperFacade;
+import no.nav.tps.forvalteren.common.message.MessageProvider;
+import no.nav.tps.forvalteren.domain.jpa.Adresse;
+import no.nav.tps.forvalteren.domain.jpa.IdentHistorikk;
+import no.nav.tps.forvalteren.domain.jpa.Person;
+import no.nav.tps.forvalteren.domain.jpa.Relasjon;
+import no.nav.tps.forvalteren.domain.jpa.Statsborgerskap;
+import no.nav.tps.forvalteren.domain.rs.RsPersonKriteriumRequest;
+import no.nav.tps.forvalteren.domain.rs.dolly.RsOppdaterPersonResponse;
+import no.nav.tps.forvalteren.domain.rs.dolly.RsPersonBestillingKriteriumRequest;
+import no.nav.tps.forvalteren.repository.jpa.PersonRepository;
+import no.nav.tps.forvalteren.service.command.exceptions.TpsfFunctionalException;
+import no.nav.tps.forvalteren.service.command.exceptions.TpsfTechnicalException;
+import no.nav.tps.forvalteren.service.command.testdata.opprett.OpprettPersonerOgSjekkMiljoeService;
+import no.nav.tps.forvalteren.service.command.testdata.opprett.RandomAdresseService;
+import no.nav.tps.forvalteren.service.command.testdata.utils.HentDatoFraIdentService;
 
 @Service
 @RequiredArgsConstructor
@@ -37,6 +43,7 @@ public class EndrePersonBestillingService {
     private final HentDatoFraIdentService hentDatoFraIdentService;
     private final MapperFacade mapperFacade;
     private final MessageProvider messageProvider;
+    private final OpprettPersonerOgSjekkMiljoeService opprettPersonerOgSjekkMiljoeService;
 
     public RsOppdaterPersonResponse execute(String ident, RsPersonBestillingKriteriumRequest request) {
 
@@ -48,6 +55,9 @@ public class EndrePersonBestillingService {
 
         validateUpdateRequest(request, person);
 
+        if (isNotBlank(request.getIdenttype())) {
+            person = swapIdent(request, person);
+        }
         mapperFacade.map(request, person);
 
         updateAdresse(request, person);
@@ -69,6 +79,24 @@ public class EndrePersonBestillingService {
         identer.add(0, RsOppdaterPersonResponse.IdentTuple.builder().ident(person.getIdent()).build());
 
         return RsOppdaterPersonResponse.builder().identTupler(identer).build();
+    }
+
+    private Person swapIdent(RsPersonBestillingKriteriumRequest request, Person person) {
+
+        RsPersonKriteriumRequest personKriterier = extractMainPerson(request);
+        try {
+            Person nyPerson = opprettPersonerOgSjekkMiljoeService.createNyeIdenter(personKriterier).get(0);
+            nyPerson.getIdentHistorikk().add(IdentHistorikk.builder()
+                    .person(nyPerson)
+                    .aliasPerson(person)
+                    .historicIdentOrder(1)
+                    .regdato(LocalDateTime.now())
+                    .build());
+
+            return nyPerson;
+        } catch (RuntimeException e) {
+            throw new TpsfTechnicalException("Feilet å opprette person med identtype " + request.getIdenttype(), e);
+        }
     }
 
     private void validateUpdateRequest(RsPersonBestillingKriteriumRequest request, Person person) {
