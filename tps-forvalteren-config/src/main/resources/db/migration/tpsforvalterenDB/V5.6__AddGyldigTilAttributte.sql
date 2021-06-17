d224ee6006b10a1046473fb1e0b9abd00bc52073
stigus <stian.gustavsson@nav.no>
2021-06-17 13:26:09 +0200

    Lagt til Gyldig_Til_Dato attributt

---
+++ tps-forvalteren-config/src/main/resources/db/migration/tpsforvalterenDB/V5.6__AddGyldigTilAttributte.sql
@@ -0,0 +1,8 @@
+-------------------------------
+-- A L T E R     T A B L E S --
+-------------------------------
+ALTER TABLE T_GATEADRESSE
+ADD GYLDIG_TIL_DATO DATE;
+
+ALTER TABLE T_MATRIKKELADRESSE
+ADD GYLDIG_TIL_DATO DATE;
